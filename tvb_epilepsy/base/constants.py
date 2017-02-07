79c3b4773a2df8bff4277186ed8766a169c1df2b
dionperd <dionperd@gmail.com>
2017-02-07 17:29:19 +0100

    Renamed and refactored module so that there are no conflicts with tvb paths. Corrected, debugged and tested the notebook example as well. Small improvements here and there. Now adding also the refactored functions...

---
+++ tvb_epilepsy/base/constants.py
@@ -0,0 +1,56 @@
+"""
+@version $Id: constants.py 1651 2016-09-01 17:47:05Z denis $
+
+Various configurations witch might or might not be system based, should be specified here.
+"""
+
+import os
+import platform
+from datetime import datetime
+
+USER_HOME = os.path.expanduser("~")
+
+if platform.node()=='dionperdMBP':
+    FOLDER_VEP = os.path.join(USER_HOME, 'CBR','VEP')
+    DATA_EPISENSE = os.path.join(USER_HOME, 'CBR','svn','episense', 'demo-data')
+    DATA_TVB = os.path.join(USER_HOME, 'CBR','svn','tvb', 'tvb-data', 'tvb-data')
+else:
+    FOLDER_VEP = os.path.join(USER_HOME, 'VEP')
+    DATA_EPISENSE = os.path.join(USER_HOME, 'CBR_software','svn-episense', 'demo-data')
+    DATA_TVB = os.path.join(USER_HOME,'CBR_software', 'svn-tvb', 'tvb-data', 'tvb-data')
+
+if not (os.path.isdir(FOLDER_VEP)):
+    os.mkdir(FOLDER_VEP)
+
+# Folder where input data will be 
+FOLDER_DATA = os.path.join(FOLDER_VEP, 'data')
+
+# Folder where logs will be written
+FOLDER_LOGS = os.path.join(FOLDER_VEP, 'logs'+datetime.strftime(datetime.now(), '%Y-%m-%d_%H-%M')  )
+
+# Folder where results will be saved
+FOLDER_RES = os.path.join(FOLDER_VEP, 'results'+datetime.strftime(datetime.now(), '%Y-%m-%d_%H-%M') ) 
+if not (os.path.isdir(FOLDER_RES)):
+        os.mkdir(FOLDER_RES)
+# Figures related settings:
+VERY_LARGE_SIZE = (30, 15)
+LARGE_SIZE = (20, 15)
+SMALL_SIZE = (15, 10)
+FOLDER_FIGURES = os.path.join(FOLDER_VEP, 'figures'+datetime.strftime(datetime.now(), '%Y-%m-%d_%H-%M') ) 
+if not (os.path.isdir(FOLDER_FIGURES)):
+        os.mkdir(FOLDER_FIGURES)
+FIG_FORMAT = 'png'
+SAVE_FLAG = True
+SHOW_FLAG = False
+MOUSEHOOVER = False
+
+# Simulation and data read folder amd flags:
+MODEL = '6v'
+SIMULATION_MODE = 'tvb'  # 'ep' or 'tvb
+DATA_MODE = 'ep'  # 'ep' or 'tvb'
+
+# Normalization configuration:
+WEIGHTS_NORM_PERCENT = 95
+
+NOISE_SEED = 42
+
---
+++ tvb_epilepsy/tvb_api/readers_tvb.py
@@ -0,0 +1,80 @@
+"""
+@version $Id: readers_tvb.py 1524 2016-07-27 14:11:08Z lia.domide $
+
+Read VEP related entities from TVB format and data-structures
+"""
+
+from tvb.basic.profile import TvbProfile
+TvbProfile.set_profile(TvbProfile.LIBRARY_PROFILE)
+
+import os
+from tvb.datatypes import connectivity, surfaces, region_mapping, sensors, structural, projections
+from tvb_epilepsy.base.model_vep import Connectivity, Surface, Sensors, Head
+from tvb_epilepsy.base.readers import ABCReader
+
+class TVBReader(ABCReader):
+        
+    def read_connectivity(self, path):
+        tvb_conn = connectivity.Connectivity.from_file(path)
+        return Connectivity(tvb_conn.weights, tvb_conn.tract_lengths,
+                            tvb_conn.region_labels, tvb_conn.centres,
+                            tvb_conn.hemispheres, tvb_conn.orientations, tvb_conn.areas)
+
+    def read_cortical_surface(self, path):
+        tvb_srf = surfaces.CorticalSurface.from_file(path)
+        return Surface(tvb_srf.vertices, tvb_srf.triangles,
+                       tvb_srf.vertex_normals, tvb_srf.triangle_normals)
+
+    def read_region_mapping(self, path):
+        tvb_rm = region_mapping.RegionMapping.from_file(path)
+        return tvb_rm.array_data
+
+    def read_volume_mapping(self, path):
+        tvb_vm = region_mapping.RegionVolumeMapping.from_file(path)
+        return tvb_vm.array_data
+
+    def read_t1(self, path):
+        tvb_t1 = structural.StructuralMRI.from_file(path)
+        return tvb_t1.array_data
+
+    def read_sensors(self, path, s_type):
+        if s_type == Sensors.TYPE_EEG:
+            tvb_sensors = sensors.SensorsEEG.from_file(path)
+        elif s_type == Sensors.TYPE_MEG:
+            tvb_sensors = sensors.SensorsMEG.from_file(path)
+        else:
+            tvb_sensors = sensors.SensorsInternal.from_file(path)
+        return Sensors(tvb_sensors.labels, tvb_sensors.locations, tvb_sensors.orientations, s_type)
+
+    def read_projection(self, path, s_type):
+        if s_type == Sensors.TYPE_EEG:
+            tvb_prj = projections.ProjectionSurfaceEEG.from_file(path)
+        elif s_type == Sensors.TYPE_MEG:
+            tvb_prj = projections.ProjectionSurfaceMEG.from_file(path)
+        else:
+            tvb_prj = projections.ProjectionSurfaceSEEG.from_file(path)
+        return tvb_prj.projection_data
+
+    def read_head(self, root_folder, name=''):
+        conn = self.read_connectivity(os.path.join(root_folder, "connectivity", "connectivity_76.zip"))
+        srf = self.read_cortical_surface(os.path.join(root_folder, "surfaceData", "cortex_16384.zip"))
+        rm = self.read_region_mapping(os.path.join(root_folder, "regionMapping", "regionMapping_16k_76.txt"))
+        vm = None
+        t1 = None
+
+        s = self.read_sensors(os.path.join(root_folder, "sensors", "eeg_brainstorm_65.txt"), Sensors.TYPE_EEG)
+        pm = self.read_projection(os.path.join(root_folder, "projectionMatrix", "projection_eeg_65_surface_16k.npy"),
+                                  Sensors.TYPE_EEG)
+        eeg_sensors_dict = {s: pm}
+
+        s = self.read_sensors(os.path.join(root_folder, "sensors", "meg_brainstorm_276.txt"), Sensors.TYPE_MEG)
+        pm = self.read_projection(os.path.join(root_folder, "projectionMatrix", "projection_meg_276_surface_16k.npy"),
+                                  Sensors.TYPE_MEG)
+        meg_sensors_dict = {s: pm}
+
+        s = self.read_sensors(os.path.join(root_folder, "sensors", "seeg_588.txt"), Sensors.TYPE_SEEG)
+        pm = self.read_projection(os.path.join(root_folder, "projectionMatrix", "projection_seeg_588_surface_16k.npy"),
+                                  Sensors.TYPE_SEEG)
+        seeg_sensors_dict = {s: pm}
+
+        return Head(conn, srf, rm, vm, t1, name, eeg_sensors_dict, meg_sensors_dict, seeg_sensors_dict)
