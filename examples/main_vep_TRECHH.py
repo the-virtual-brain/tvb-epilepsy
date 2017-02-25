Refactoring, Modyfing, Debugging, Correcting DIsassociated TVB equilibrium computation from tvb_epilepsy/basic Now the model is created outside the simulator class Added checks for correct equilibrium computation by evaluating model.dfun

examples/main_vep_TRECHH.py
@@ -19,7 +19,7 @@ from tvb_epilepsy.base.plot_tools import plot_head, plot_hypothesis, plot_sim_re
 
 SHOW_FLAG = False
 SAVE_FLAG = True
-SHOW_FLAG_SIM = True
+SHOW_FLAG_SIM = False
 
 #Modify data folders for this example:
 DATA_TRECHH = '/Users/dionperd/Dropbox/Work/VBtech/DenisVEP/Results/TRECHH'
@@ -90,9 +90,7 @@ if __name__ == "__main__":
             print projection.shape
             sensorsSEEG.append(sensors)
             projections.append(projection) 
-    plot_head(head, save_flag=SAVE_FLAG, show_flag=SHOW_FLAG, 
-              figure_dir=FOLDER_FIGURES, figsize=VERY_LARGE_SIZE)
-
+    #plot_head(head, save_flag=SAVE_FLAG, show_flag=SHOW_FLAG, figure_dir=FOLDER_FIGURES, figsize=VERY_LARGE_SIZE)
              
 #--------------------------Hypothesis and LSA-----------------------------------
        
@@ -132,7 +130,7 @@ if __name__ == "__main__":
     seizure_indices = np.array([50], dtype=np.int32)
     
     hyp_ep = Hypothesis(head.number_of_regions, head.connectivity.normalized_weights, \
-                        "EP Hypothesis", x1eq_mode = "linTaylor")  #"optimize"
+                        "EP Hypothesis", x1eq_mode="optimize")  #"linTaylor"
     hyp_ep.K = 0.1*hyp_ep.K # 1.0/np.max(head.connectivity.normalized_weights)
     # iE = np.array([1,    3,   7,   8,  15,  16,  25,  50, 88,  89])
     # E = 0*np.array(iE, dtype=np.float32)
@@ -142,89 +140,99 @@ if __name__ == "__main__":
     # E[8]=0.425
     # E[9]=0.900
     iE = np.array([50])
-    E = np.array([0.9], dtype=numpy.float32)
+    E = np.array([0.25], dtype=numpy.float32)
+    # iE = np.array(range(hyp_ep.n_regions))
+    # E = (-0.5 * np.ones((1,hyp_ep.n_regions))).tolist()
     hyp_ep.configure_e_hypothesis(iE, E, seizure_indices)
     logger.debug(str(hyp_ep))
-    plot_hypothesis(hyp_ep, head.connectivity.region_labels,
-                    save_flag=SAVE_FLAG, show_flag=SHOW_FLAG, 
-                    figure_dir=FOLDER_FIGURES, figsize=VERY_LARGE_SIZE)
+    #plot_hypothesis(hyp_ep, head.connectivity.region_labels, save_flag=SAVE_FLAG, show_flag=SHOW_FLAG,
+    #               figure_dir=FOLDER_FIGURES, figsize=VERY_LARGE_SIZE)
 
-#    hyp_exc = Hypothesis(head.number_of_regions, head.connectivity.normalized_weights, "x0 Hypothesis")
-#    #hyp_exc = cp.deepcopy(hyp_ep)
-#    #hyp_exc.name = "EP & x0 Hypothesis"
-#    hyp_exc.K = 0.1*hyp_exc.K 
-#    ii = np.array(range(head.number_of_regions), dtype=np.int32)
-#    ix0 = ii#np.delete(ii, 0)
-#    x0 = X0_DEF*ix0
-#    x0[50] = 0.8
-#    hyp_exc.configure_x0_hypothesis(ix0, x0, seizure_indices)
-#    logger.debug(str(hyp_exc))
-#    plot_hypothesis(hyp_exc, head.connectivity.region_labels,
-#                    save_flag=SAVE_FLAG, show_flag=True, 
-#                    figure_dir=FOLDER_FIGURES, figsize=VERY_LARGE_SIZE)
+    # hyp_exc = Hypothesis(head.number_of_regions, head.connectivity.normalized_weights,
+    #                     "x0 Hypothesis", x1eq_mode="optimize")  #"optimize" or "linTaylor"
+    # hyp_exc.K = 0.1 * hyp_exc.K
+    # ix0 =range(head.number_of_regions)
+    # x0 = (X0_DEF * numpy.ones((len(ix0),), dtype='float32')).tolist()
+
+    hyp_exc = cp.deepcopy(hyp_ep)
+    hyp_exc.name = "EP & x0 Hypothesis"
+    hyp_exc.x1eq_mode = "linTaylor"
+    ix0 = [51, 52]
+    x0 = (0.5 * numpy.ones((len(ix0),), dtype='float32')).tolist()
+
+    hyp_exc.configure_x0_hypothesis(ix0, x0, seizure_indices)
+    logger.debug(str(hyp_exc))
+    plot_hypothesis(hyp_exc, head.connectivity.region_labels,
+                    save_flag=SAVE_FLAG, show_flag=True,
+                    figure_dir=FOLDER_FIGURES, figsize=VERY_LARGE_SIZE)
 
     
 #------------------------------Simulation--------------------------------------
 
+    #We don't want any time delays for the moment
+    head.connectivity.tract_lengths *= 0.0
+
     # Set time scales (all times should be in msecs):
     (fs, dt, fsAVG, scale_time, sim_length, monitor_period,
-     n_report_blocks, hpf_fs, hpf_low, hpf_high) = set_time_scales(fs=4096.0, dt=None, time_length=60000.0,
+     n_report_blocks, hpf_fs, hpf_low, hpf_high) = set_time_scales(fs=2*4096.0, dt=None, time_length=1000.0,
                                                                    scale_time=2.0, scale_fsavg=2.0,
                                                                    hpf_low=None, hpf_high=None,
-                                                                   report_percentage=10.0)
+                                                                   report_every_n_monitor_steps=10.0)
 
     #Now simulate and plot for each hypothesis
     for hyp in (hyp_ep,): # ,hyp_exc #length=30000
 
         # Choose the model and build it on top of the specific hypothesis, adjust parameters:
-        model = '11D'  # '6D', '2D', '11D', 'tvb'
+        model = '6D'  # '6D', '2D', '11D', 'tvb'
         if model == '6D':
-            model = build_ep_6sv_model(hyp_ep, variables_of_interest=["y3 - y0", "y2"], zmode=numpy.array("lin"))
-            vars = ['lfp', 'x1', 'y1', 'z', 'x2', 'y2', 'g']
+            model = build_ep_6sv_model(hyp_ep, zmode=numpy.array("lin"))
             # model.tau0 = 2857.0 # default = 2857.0
-            model.tau1 = scale_time * model.tau1  # default = 0.25
+            model.tau1 *= scale_time  # default = 0.25
         elif model == '2D':
-            model = build_ep_2sv_model(hyp_ep, variables_of_interest=["y0", "y1"], zmode=numpy.array("lin"))
-            vars = ['x1', 'z']
+            model = build_ep_2sv_model(hyp_ep, zmode=numpy.array("lin"))
             # model.tau0 = 2857.0 # default = 2857.0
-            model.tau1 = scale_time * model.tau1  # default = 0.25
+            model.tau1 *= scale_time  # default = 0.25
         elif model == '11D':
-            model = build_ep_11sv_model(hyp_ep, variables_of_interest=["y3 - y0", "y2"], zmode=numpy.array("lin"))
-            vars = ['lfp', 'x1', 'y1', 'z', 'x2', 'y2', 'g', 'x0ts', 'slopeTS', 'Iext1ts', 'Iext2ts', 'Kts']
+            model = build_ep_11sv_model(hyp_ep, zmode=numpy.array("lin"))
             # model.tau0 = 10000 # default = 10000
-            model.tau1 = scale_time * model.tau1  # default = 0.25
+            model.tau1 *= scale_time  # default = 0.25
             model.slope = 0.25
             model.pmode = np.array("z")  # "z","g","z*g", default="cons
         elif model == 'tvb':
-            model = build_tvb_model(hyp_ep, variables_of_interest=["y3 - y0", "y2"], zmode=numpy.array("lin"))
-            vars = ['lfp', 'x1', 'y1', 'z', 'x2', 'y2', 'g']
-            model.tt = scale_time * 0.25 * model.tt  # default = 1.0
-            # model.r = 1.0/10000  # default = 1.0 / 2857.0
+            model = build_tvb_model(hyp_ep, zmode=numpy.array("lin"))
+            model.tt *= scale_time * 0.25  # default = 1.0
+            # model.r = 1.0/2857.0  # default = 1.0 / 2857.0
 
         # Setup and configure the simulator according to the specific model (and, therefore, hypothesis)
-        (simulator_instance, sim_settings) = setup_simulation(model, dt, sim_length, monitor_period,
-                                                              noise_instance=None, noise_intensity=None,
-                                                              monitor_exr=None)
+        # Good choices for noise and monitor expressions are made in this helper function
+        # It returns name strings for the variables of interest (vois) accordingly
+        # monitor_expr and vois have to be list of strings of the same length
+        # noise_intensity overwrites the one inside noise_instance if given additionally
+        # monitor_period overwrites the one inside monitor_instance if given additionally
+        (simulator_instance, sim_settings, vois) = setup_simulation(model, dt, sim_length, monitor_period,
+                                                                    monitor_expr=None, monitors_instance=None,
+                                                                    noise_instance=None, noise_intensity=10 ** -8,
+                                                                    variables_names=None)
 
-        sim = simulator_instance.config_simulation(head, sim_settings)
+        sim = simulator_instance.config_simulation(head, settings=sim_settings)
 
         #Launch simulation
         ttavg, tavg_data = simulator_instance.launch_simulation(sim, hyp, n_report_blocks=n_report_blocks)
         logger.info("Simulated signal return shape: " + str(tavg_data.shape))
-        logger.info("Time: " + str(ttavg[0]) + " - " + str(ttavg[-1]))
+        logger.info("Time: " + str(scale_time*ttavg[0]) + " - " + str(scale_time*ttavg[-1]))
         logger.info("Values: " + str(tavg_data.min()) + " - " + str(tavg_data.max()))
 
         #Pack results into a dictionary, high pass filter, and compute SEEG
         res = dict()
-        for iv in range(len(vars)):
-            res[vars[iv]] = numpy.array(tavg_data[:, iv, :, 0], dtype='float32')
-        res['seeg'] = []
+        for iv in range(len(vois)):
+            res[vois[iv]] = numpy.array(tavg_data[:, iv, :, 0], dtype='float32')
 
+        res['seeg'] = []
         if isinstance(sim.model, EpileptorDP2D):
             for i in range(len(projections)):
-                res['seeg'+str(i)] = numpy.dot(res['x1'], projections[i].T)
+                res['seeg'+str(i)] = numpy.dot(res['z'], projections[i].T)
         else:
-            hpf = numpy.empty((ttavg.size, hyp.n_regions)).astype(numpy.float32)
+            res['hpf'] = numpy.empty((ttavg.size, hyp.n_regions)).astype(numpy.float32)
             for i in range(hyp.n_regions):
                 res['hpf'][:, i] = filter_data(res['lfp'][:, i], hpf_low, hpf_high, hpf_fs)
                 res['hpf'] = numpy.array(res['hpf'], dtype='float32')
@@ -237,9 +245,8 @@ if __name__ == "__main__":
         del ttavg, tavg_data
 
         #Plot results
-        plot_sim_results(model, hyp_ep, head, res, sensorsSEEG, SHOW_FLAG_SIM)
+        plot_sim_results(model, hyp_ep, head, res, sensorsSEEG)
 
         #Save results
         res['time_units'] = 'msec'
         savemat(os.path.join(FOLDER_RES, hyp.name + "_ts.mat"), res)
-

tvb_epilepsy/base/equilibrium_computation.py
@@ -3,7 +3,7 @@
 
 Module to compute the resting equilibrium point of a Virtual Epileptic Patient module
 """
-
+import warnings
 import numpy
 from scipy.optimize import root
 from sympy import symbols, exp, solve, lambdify
@@ -15,22 +15,22 @@ from tvb_epilepsy.base.constants import X1_DEF, X1_EQ_CR_DEF, X0_DEF, X0_CR_DEF
 
 def x1eq_def(X1_DEF, X1_EQ_CR_DEF, n_regions):
     #The default initial condition for x1 equilibrium search
-    return numpy.repeat(numpy.array((X1_EQ_CR_DEF + X1_DEF) / 2.0), n_regions)
+    return (X1_EQ_CR_DEF + X1_DEF) / 2.0 * numpy.ones((1,n_regions), dtype='float32')
 
 
 def x1_lin_def(X1_DEF, X1_EQ_CR_DEF, n_regions):
     # The point of the linear Taylor expansion
-    return numpy.repeat(numpy.array((X1_EQ_CR_DEF + X1_DEF) / 2.0), n_regions)
+    return (X1_EQ_CR_DEF + X1_DEF) / 2.0 * numpy.ones((1,n_regions), dtype='float32')
 
 
 def fx1_2d_calc(x1, z=0, y0=0, Iext1=0):
     x12 = x1 ** 2
-    return -x1 * x12 - 2 * x12 - z + y0 + Iext1
+    return y0 - x1 * x12 - 2 * x12 - z + Iext1
 
 
-def fx1_6d_calc(x1, z=0, y0=0, Iext1=0):
+def fx1_6d_calc(x1, z=0, y1=0, Iext1=0):
     x12 = x1 ** 2
-    return -x1 * x12 + 3 * x12 - z + y0 + Iext1
+    return y1 - x1 * x12 + 3 * x12 - z + Iext1
 
 
 def fz_lin_calc(x1, x0, x0cr, r, z=0, coupl=0):
@@ -45,28 +45,28 @@ def zeq_2d_calc(x1eq, y0, Iext1):
     return fx1_2d_calc(x1eq, z=0, y0=y0, Iext1=Iext1)
 
 
-def zeq_6d_calc(x1eq, y0, Iext1):
-    return fx1_6d_calc(x1eq, z=0, y0=y0, Iext1=Iext1)
+def zeq_6d_calc(x1eq, y1, Iext1):
+    return fx1_6d_calc(x1eq, z=0, y1=y1, Iext1=Iext1)
 
 
-def y1eq_calc(x1eq, d=5.0):
-    return 1 - d * x1eq ** 2
+def y1eq_calc(x1eq, yc, d=5.0):
+    return yc - d * x1eq ** 2
 
 
 def pop2eq_calc(x1eq, zeq, Iext2):
     shape = x1eq.shape
-    type = x1eq.dtype
+    x1_type = x1eq.dtype
     # g_eq = 0.1*x1eq (1)
     # y2eq = 0 (2)
-    y2eq = numpy.zeros(shape, dtype=type)
+    y2eq = numpy.zeros(shape, dtype=x1_type)
     # -x2eq**3 + x2eq -y2eq+2*g_eq-0.3*(zeq-3.5)+Iext2 =0=> (1),(2)
     # -x2eq**3 + x2eq +2*0.1*x1eq-0.3*(zeq-3.5)+Iext2 =0=>
     # p3        p1                   p0
     # -x2eq**3 + x2eq +0.2*x1eq-0.3*(zeq-3.5)+Iext2 =0
     p0 = 0.2 * x1eq - 0.3 * (zeq - 3.5) + Iext2
-    x2eq = numpy.zeros(shape, dtype=type)
+    x2eq = numpy.zeros(shape, dtype=x1_type)
     for i in range(shape[1]):
-        x2eq[0 ,i] = numpy.min(numpy.real(numpy.roots([-1.0, 0.0, 1.0, p0[0 ,i]])))
+        x2eq[0 ,i] = numpy.min(numpy.real(numpy.roots([-1.0, 0.0, 1.0, p0[0, i]])))
     return x2eq, y2eq
 
 
@@ -95,51 +95,66 @@ def geq_calc(x1eq):
 
 def x1eq_x0_hypo_optimize_fun(x, ix0, iE, x1EQ, zEQ, x0, x0cr, rx0, y0, Iext1, K, w):
 
+    x = numpy.expand_dims(x,1).T
+
     no_x0 = len(ix0)
     no_e = len(iE)
 
-    type = x1EQ.dtype
-    i_e = numpy.ones((no_e,1), dtype=type)
-    i_x0 = numpy.ones((no_x0,1), dtype=type)
+    x1_type = x1EQ.dtype
+    i_e = numpy.ones((no_e,1), dtype=x1_type)
+    i_x0 = numpy.ones((no_x0,1), dtype=x1_type)
 
     #Coupling                        to   from           from                    to
     w_e_to_e = numpy.sum(numpy.dot(w[iE][:,iE],    numpy.dot(i_e, x1EQ[:,iE]) - numpy.dot(i_e, x1EQ[:,iE]).T), axis=1)
-    w_x0_to_e = numpy.sum(numpy.dot(w[iE][:, ix0], numpy.dot(i_e, x0) - numpy.dot(i_x0, x1EQ[:,iE]).T), axis=1)
+    w_x0_to_e = numpy.sum(numpy.dot(w[iE][:, ix0], (numpy.dot(i_e, x0) - numpy.dot(i_x0, x1EQ[:,iE]).T).T), axis=1)
 
-    w_e_to_x0 = numpy.sum(numpy.dot(w[ix0][:,iE],  numpy.dot(i_x0, x1EQ[:,iE]) - numpy.dot(i_e, x0).T), axis=1)
+    w_e_to_x0 = numpy.sum(numpy.dot(w[ix0][:, iE], (numpy.dot(i_x0, x1EQ[:, iE]) - numpy.dot(i_e, x0).T).T), axis=1)
     w_x0_to_x0 = numpy.sum(numpy.dot(w[ix0][:,ix0], numpy.dot(i_x0, x0) - numpy.dot(i_x0, x0).T), axis=1)
 
-    fun = numpy.array(x1EQ.shape)
+    fun = numpy.zeros(x1EQ.shape).astype(x1_type)
     #Known x1eq, unknown x0:
-    fun[iE] = fz_lin_calc(x1EQ[iE], x[iE], x0cr[iE], rx0[iE], z=zEQ[iE], coupl=K[iE] * (w_e_to_e + w_x0_to_e))
+    fun[:,iE] = fz_lin_calc(x1EQ[:,iE], x[:,iE], x0cr[:,iE], rx0[:,iE], z=zEQ[:,iE],
+                            coupl=K[:,iE] * (w_e_to_e + w_x0_to_e)).astype(x1_type)
     # Known x0, unknown x1eq:
-    fun[ix0] = fz_lin_calc(x[ix0], x0, x0cr[ix0], rx0[ix0], z=zeq_2d_calc(x[ix0], y0[ix0], Iext1[ix0]),
-                           coupl=K[ix0] * (w_e_to_x0 + w_x0_to_x0))
+    fun[:,ix0] = fz_lin_calc(x[:, ix0], x0, x0cr[:, ix0], rx0[:, ix0],
+                             z=zeq_2d_calc(x[:, ix0], y0[:, ix0], Iext1[:, ix0]),
+                             coupl=K[:, ix0] * (w_e_to_x0 + w_x0_to_x0)).astype(x1_type)
 
-    return fun
+    # if numpy.any([numpy.any(numpy.isnan(x)), numpy.any(numpy.isinf(x)),
+    #               numpy.any(numpy.isnan(fun)), numpy.any(numpy.isinf(fun))]):
+    #     raise ValueError("nan or inf values in x or fun")
+
+    return numpy.squeeze(fun)
 
 
 def x1eq_x0_hypo_optimize_jac(x, ix0, iE, x1EQ, zEQ, x0, x0cr, rx0, y0, Iext1, K, w):
+
+    x = numpy.expand_dims(x, 1).T
 
     no_x0 = len(ix0)
     no_e = len(iE)
 
     n_regions = no_e + no_x0
 
-    type = x1EQ.dtype
+    x1_type = x1EQ.dtype
     i_x0 = numpy.ones((no_x0, 1), dtype=type)
+    i_e = numpy.ones((no_e, 1), dtype=type)
 
-    jac_e_x0e = numpy.diag(- 4 * rx0[iE])
-    jac_e_x1o = -numpy.dot(i_x0, K[:,iE]) * w[iE][:,ix0]
-    jac_x0_x0e = numpy.zeros((no_x0,no_e),dtype = type)
-    jac_x0_x1o = numpy.diag(4 + 3 * x[ix0] ** 2 + 4 * x[ix0] + K[ix0] * numpy.sum(w[ix0][:,ix0], axis=1)) \
-                 - numpy.dot(i_x0, K[:, ix0]) * w[ix0][:, ix0]
+    jac_e_x0e = numpy.diag(- 4 * rx0[:, iE]).astype(x1_type)
+    jac_e_x1o = -numpy.dot(numpy.dot(i_e, K[:,iE]), w[iE][:,ix0]).astype(x1_type)
+    jac_x0_x0e = numpy.zeros((no_x0,no_e),dtype = type).astype(x1_type)
+    jac_x0_x1o = (numpy.diag(4 + 3 * x[:, ix0] ** 2 + 4 * x[:, ix0] + K[:, ix0] * numpy.sum(w[ix0][:,ix0], axis=1)) - \
+                 numpy.dot(i_x0, K[:, ix0]) * w[ix0][:, ix0]).astype(x1_type)
 
-    jac = numpy.zeros((n_regions,n_regions), dtype=type)
-    jac[iE][:,iE] = jac_e_x0e
-    jac[iE][:, ix0] = jac_e_x1o
-    jac[ix0][:, iE] = jac_x0_x0e
-    jac[ix0][:, ix0] = jac_x0_x1o
+    jac = numpy.zeros((n_regions,n_regions), dtype=x1_type)
+    jac[numpy.ix_(iE, iE)] = jac_e_x0e
+    jac[numpy.ix_(iE, ix0)] = jac_e_x1o
+    jac[numpy.ix_(ix0, iE)] = jac_x0_x0e
+    jac[numpy.ix_(ix0, ix0)] = jac_x0_x1o
+
+    # if numpy.any([ numpy.any(numpy.isnan(x)), numpy.any(numpy.isnan(x)),
+    #                numpy.any(numpy.isnan(jac.flatten())), numpy.any(numpy.isinf(jac.flatten()))]):
+    #     raise ValueError("nan or inf values in x or jac")
 
     return jac
 
@@ -160,14 +175,17 @@ def x1eq_x0_hypo_optimize(ix0, iE, x1EQ, zEQ, x0, x0cr, rx0, y0, Iext1, K, w):
                method='lm', jac=x1eq_x0_hypo_optimize_jac, tol=10**(-6), callback=None, options=None) #method='hybr'
 
     if sol.success:
-        x1EQ[:,ix0] = sol.x[:, ix0]
-        return x1EQ
+        x1EQ[:, ix0] = sol.x[ix0]
+        if numpy.any([numpy.any(numpy.isnan(sol.x)), numpy.any(numpy.isinf(sol.x))]):
+            raise ValueError("nan or inf values in solution x\n" + sol.message)
+        else:
+            return x1EQ
     else:
         raise ValueError(sol.message)
 
 
 
-def x1eq_x0_hypo_linTaylor(ix0,iE,x1EQ,zEQ,x0,x0cr,rx0,y0,Iext1,K,w):
+def x1eq_x0_hypo_linTaylor(ix0, iE, x1EQ, zEQ, x0, x0cr, rx0, y0, Iext1, K, w):
 
     no_x0 = len(ix0)
     no_e = len(iE)
@@ -180,46 +198,50 @@ def x1eq_x0_hypo_linTaylor(ix0,iE,x1EQ,zEQ,x0,x0cr,rx0,y0,Iext1,K,w):
 
     #Prepare linear system to solve:
 
+    x1_type = x1EQ.dtype
+
     #The point of the linear Taylor expansion
-    x1LIN = x1_lin_def(X1_DEF, X1_EQ_CR_DEF, n_regions)
+    x1LIN = x1_lin_def(X1_DEF, X1_EQ_CR_DEF, n_regions).astype(x1_type)
 
     # For regions of fixed equilibria:
-    ii_e = numpy.ones((1, no_e), dtype=numpy.float32)
+    ii_e = numpy.ones((1, no_e), dtype=x1_type)
     we_to_e = numpy.expand_dims(numpy.sum(w[iE][:, iE] * (numpy.dot(ii_e.T, x1_eq) -
-                                                    numpy.dot(x1_eq.T, ii_e)), axis=1), 1).T
-    wx0_to_e = -x1_eq * numpy.expand_dims(numpy.sum(w[ix0][:, iE], axis=0), 0)
-    be = 4.0 * (x1_eq + x0cr[:, iE]) - z_eq - K[:, iE] * (we_to_e + wx0_to_e)
+                                                          numpy.dot(x1_eq.T, ii_e)), axis=1), 1).T.astype(x1_type)
+    wx0_to_e = x1_eq * numpy.expand_dims(numpy.sum(w[ix0][:, iE], axis=0), 0).astype(x1_type)
+    be = 4.0 * (x1_eq + x0cr[:, iE]) - z_eq - K[:, iE] * (we_to_e - wx0_to_e)
 
     # For regions of fixed x0:
-    ii_x0 = numpy.ones((1, no_x0), dtype=numpy.float32)
-    we_to_x0 = numpy.expand_dims(numpy.sum(w[ix0][:, iE] * numpy.dot(ii_x0.T, x1_eq), axis=1), 1).T
+    ii_x0 = numpy.ones((1, no_x0), dtype=x1_type)
+    we_to_x0 = numpy.expand_dims(numpy.sum(w[ix0][:, iE] * numpy.dot(ii_x0.T, x1_eq), axis=1), 1).T.astype(x1_type)
     bx0 = 4.0 * (x0cr[:, ix0] - rx0[:, ix0] * x0) - y0[:, ix0] - Iext1[:, ix0] \
           - 2.0 * x1LIN[:, ix0] ** 3 - 2.0 * x1LIN[:, ix0] ** 2 - K[:, ix0] * we_to_x0
 
     # Concatenate B vector:
-    b = -numpy.concatenate((be, bx0), axis=1).T
+    b = -numpy.concatenate((be, bx0), axis=1).T.astype(x1_type)
 
     # From-to Epileptogenicity-fixed regions
     # ae_to_e = -4 * numpy.eye( no_e, dtype=numpy.float32 )
-    ae_to_e = -4 * numpy.diag(rx0[0, iE])
+    ae_to_e = -4 * numpy.diag(rx0[0, iE]).astype(x1_type)
 
     # From x0-fixed regions to Epileptogenicity-fixed regions
     ax0_to_e = -numpy.dot(K[:, iE].T, ii_x0) * w[iE][:, ix0]
 
     # From Epileptogenicity-fixed regions to x0-fixed regions
-    ae_to_x0 = numpy.zeros((no_x0, no_e), dtype=numpy.float32)
+    ae_to_x0 = numpy.zeros((no_x0, no_e), dtype=x1_type)
 
     # From-to x0-fixed regions
-    ax0_to_x0 = numpy.diag((4.0 + 3.0 * (x1LIN[:, ix0] ** 2 + 4.0 * x1LIN[:, ix0]) +
-                K[0, ix0] * numpy.expand_dims(numpy.sum(w[ix0][:, ix0], axis=0), 0)).T[:, 0]) \
-                - numpy.dot(K[:, ix0].T, ii_x0) * w[ix0][:, ix0]
+    ax0_to_x0 = numpy.diag( (4.0 + 3.0 * x1LIN[:, ix0] ** 2 + 4.0 * x1LIN[:, ix0] +
+                K[0, ix0] * numpy.expand_dims(numpy.sum(w[ix0][:, ix0], axis=0), 0)).T[:, 0]) - \
+                numpy.dot(K[:, ix0].T, ii_x0) * w[ix0][:, ix0]
 
     # Concatenate A matrix
     a = numpy.concatenate((numpy.concatenate((ae_to_e, ax0_to_e), axis=1),
-                           numpy.concatenate((ae_to_x0, ax0_to_x0), axis=1)), axis=0)
+                           numpy.concatenate((ae_to_x0, ax0_to_x0), axis=1)), axis=0).astype(x1_type)
 
     # Solve the system
     x = numpy.dot(numpy.linalg.inv(a), b).T
+    if numpy.any([numpy.any(numpy.isnan(x)), numpy.any(numpy.isnan(x))]):
+        raise ValueError("nan or inf values in solution x")
 
     # Unpack solution:
     # The equilibria of the regions with fixed E have not changed:
@@ -241,6 +263,7 @@ def x0cr_rx0_calc(y0, Iext1, epileptor_model = "2d", zmode = numpy.array("lin"),
     if isinstance(epileptor_model,EpileptorDP2D) or  epileptor_model=="2d":
         #...for the 2D permittivity coupling approximation, Proix et al 2014
         fx1 = x1 ** 3 + 2 * x1 ** 2
+
     else:
         #...or for the original (>=6D) epileptor
         fx1 = x1 ** 3 - 3 * x1 ** 2
@@ -252,19 +275,21 @@ def x0cr_rx0_calc(y0, Iext1, epileptor_model = "2d", zmode = numpy.array("lin"),
     if zmode == 'lin':
         #...for linear...
         fz = 4 * (x1 - r * x0 + x0cr) - z
+
     elif zmode == 'sig':
         #...and sigmoidal versions
         fz = 3/(1 + exp(-10 * (x1 + 0.5))) - r * x0 + x0cr - z
+
     else:
         raise ValueError('zmode is neither "lin" nor "sig"')
 
     #Solve the fz expression for rx0 and x0cr, assuming the following two points (x1eq,x0) = [(-5/3,0.0),(-4/3,1.0)]...
     #...and WITHOUT COUPLING
     fz_sol = solve([fz.subs([(x1, x1rest), (x0, x0def), (z, z.subs(x1, x1rest))]),
-                       fz.subs([(x1, x1cr), (x0, x0cr_def), (z, z.subs(x1, x1cr))])], r, x0cr)
+                    fz.subs([(x1, x1cr), (x0, x0cr_def), (z, z.subs(x1, x1cr))])], r, x0cr)
 
     #Convert the solution of x0cr from expression to function that accepts numpy arrays as inputs:
-    x0cr = lambdify((y01,I1),fz_sol[x0cr],'numpy')
+    x0cr = lambdify((y01,I1), fz_sol[x0cr], 'numpy')
 
     #Compute the actual x0cr now given the inputs y0 and Iext1
     x0cr = x0cr(y0, Iext1)
@@ -280,7 +305,7 @@ def coupling_calc(x1, K, w):
     # Only difference coupling for the moment.
     # TODO: Extend for different coupling forms
     n_regions = x1.size
-    i_n = numpy.ones((n_regions,1), dtype='f')
+    i_n = numpy.ones((n_regions, 1), dtype='float32')
     # Coupling                         from                    to
     return K*numpy.sum(numpy.dot(w, numpy.dot(i_n, x1) - numpy.dot(i_n, x1).T), axis=1)
 
@@ -289,40 +314,66 @@ def x0_calc(x1, z, x0cr, rx0, coupl, zmode=numpy.array("lin")):
 
     if zmode == 'lin':
         return (x1 + x0cr - (z+coupl) / 4) / rx0
+
     elif zmode == 'sig':
         return (3 / (1 + numpy.exp(-10 * (x1 + 0.5))) + x0cr - z + coupl) / rx0
+
     else:
         raise ValueError('zmode is neither "lin" nor "sig"')
 
 
-def calc_equilibrium_point(epileptor_model, hypothesis):
-    from tvb_epilepsy.tvb_api.epileptor_models import EpileptorDPrealistic, EpileptorDP2D
-    from tvb.simulator.models import Epileptor
+def assert_equilibrium_point(epileptor_model, hypothesis, equilibrium_point):
 
-    #De-center them:
+    from tvb_epilepsy.tvb_api.epileptor_models import EpileptorDPrealistic
+
+    coupl = coupling_calc(hypothesis.x1EQ, epileptor_model.K.T, hypothesis.weights)
+    coupl = numpy.expand_dims(numpy.r_[coupl, 0.0 * coupl], 2).astype('float32')
+
+    dfun_max = numpy.max(epileptor_model.dfun(numpy.expand_dims(equilibrium_point, 2).astype('float32'), coupl))
+
+    if dfun_max > 10 ** -2:
+
+        if (isinstance(epileptor_model, EpileptorDPrealistic) and
+                    epileptor_model.pmode == numpy.array(['g', 'z', 'z*g'])).any():
+            warnings.warn("Equilibrium point for initial condition not accurate enough!\n" \
+                          + "max(dfun) = " + str(dfun_max))
+
+        else:
+            raise ValueError("Equilibrium point for initial condition not accurate enough!\n" \
+                             + "max(dfun) = " + str(dfun_max))
+
+
+def calc_equilibrium_point(epileptor_model, hypothesis):
+
+    from tvb_epilepsy.tvb_api.epileptor_models import EpileptorDPrealistic, EpileptorDP2D
+
     if isinstance(epileptor_model,EpileptorDP2D):
         if epileptor_model.zmode == 'sig':
             #2D approximation, Proix et al 2014
             zeq = zeq_2d_calc(hypothesis.x1EQ, epileptor_model.yc.T, epileptor_model.Iext1.T)
         else:
             zeq = hypothesis.zEQ
-        return hypothesis.x1EQ, zeq
+        equilibrium_point = numpy.r_[hypothesis.x1EQ, zeq].astype('float32')
     else:
         #all >=6D models
-        if isinstance(epileptor_model, Epileptor):
-            #tvb
-            zeq = zeq_6d_calc(hypothesis.x1EQ, epileptor_model.c.T, epileptor_model.Iext.T)
-            y1eq = y1eq_calc(hypothesis.x1EQ, epileptor_model.c.T)
-        else:
-            zeq = zeq_6d_calc(hypothesis.x1EQ, epileptor_model.yc.T, epileptor_model.Iext1.T)
-            y1eq = y1eq_calc(hypothesis.x1EQ, epileptor_model.yc.T)
+        y1eq = y1eq_calc(hypothesis.x1EQ, epileptor_model.yc.T)
+        zeq = zeq_6d_calc(hypothesis.x1EQ, y1eq, epileptor_model.Iext1.T)
+        if epileptor_model.Iext2.size == 1:
+            epileptor_model.Iext2 = epileptor_model.Iext2[0] * numpy.ones((hypothesis.n_regions, 1))
         (x2eq, y2eq) = pop2eq_calc(hypothesis.x1EQ, zeq, epileptor_model.Iext2.T)
         geq = geq_calc(hypothesis.x1EQ)
         if isinstance(epileptor_model, EpileptorDPrealistic):
-            #the 11D "realistic" simulations model
-            return hypothesis.x1EQ, y1eq, zeq, x2eq, y2eq, geq, epileptor_model.x0.T, epileptor_model.slope.T,   \
-                    epileptor_model.Iext1.T, epileptor_model.Iext2.T, epileptor_model.K.T
+            # the 11D "realistic" simulations model
+            if epileptor_model.slope.size == 1:
+                epileptor_model.slope = epileptor_model.slope[0] * numpy.ones((hypothesis.n_regions, 1))
+            slope_eq, Iext2_eq = epileptor_model.fun_slope_Iext2(zeq.T, geq.T)
+            equilibrium_point = numpy.r_[hypothesis.x1EQ, y1eq, zeq, x2eq, y2eq, geq,
+                                          epileptor_model.x0.T, slope_eq.T, epileptor_model.Iext1.T, Iext2_eq.T,
+                                          epileptor_model.K.T].astype('float32')
         else:
             #all >=6D models
-            return hypothesis.x1EQ, y1eq, zeq, x2eq, y2eq, geq
+            equilibrium_point = numpy.r_[hypothesis.x1EQ, y1eq, zeq, x2eq, y2eq, geq].astype('float32')
 
+    assert_equilibrium_point(epileptor_model, hypothesis, equilibrium_point)
+
+    return equilibrium_point