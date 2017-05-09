71c376bd9ad070a55945b29f7e6e60632a65b7ee
dionperd <dionperd@gmail.com>
2017-05-09 19:44:32 +0200

    Cleaning up main_vep and testing it Adding analyzers.py (empty for the moment)

--- examples/main_vep.py
+++ examples/main_vep.py
@@ -7,12 +7,11 @@ import numpy as np
 import copy as cp
 from scipy.io import savemat
 from tvb_epilepsy.base.constants import *
-from tvb_epilepsy.base.utils import initialize_logger, calculate_projection, set_time_scales, filter_data, \
-                                    write_object_to_h5_file, assert_equal_objects
+from tvb_epilepsy.base.utils import initialize_logger, calculate_projection, set_time_scales, filter_data
 from tvb_epilepsy.base.plot_tools import plot_head, plot_hypothesis, plot_sim_results, plot_nullclines_eq
 from tvb_epilepsy.base.hypothesis import Hypothesis
-from tvb_epilepsy.custom.read_write import read_hypothesis, read_simulation_settings, write_ts, read_ts, write_ts_epi, \
-                                           write_ts_seeg_epi, write_h5_model
+from tvb_epilepsy.custom.read_write import read_ts, write_ts_epi, write_ts_seeg_epi, write_h5_model
+
 if SIMULATION_MODE == "custom":
     from tvb_epilepsy.custom.simulator_custom import setup_simulation
 else:
@@ -136,8 +135,8 @@ if __name__ == "__main__":
     #seizure_indices = np.array([1,    3,   7,   8,  15,  16,  25,  50,   88, 89], dtype=np.int32)
     seizure_indices = np.array([50], dtype=np.int32)
     
-    hyp_ep = Hypothesis(head.number_of_regions, head.connectivity.normalized_weights, \
-                        "EP Hypothesis", x1eq_mode="optimize")  #"linTaylor"
+    hyp_ep = Hypothesis(head.number_of_regions, head.connectivity.normalized_weights, "EP Hypothesis",
+                        x1eq_mode="optimize")    #"optimize" or "linTaylor"
     # iE = np.array([1,    3,   7,   8,  15,  16,  25,  50, 88,  89])
     # E = 0*np.array(iE, dtype=np.float32)
     # E[7]=0.65
@@ -145,63 +144,51 @@ if __name__ == "__main__":
     # E[4]=0.3775
     # E[8]=0.425
     # E[9]=0.900
-    #iE = np.array([50])
-    #E = np.array([0.25], dtype=np.float32)
-    iE = np.array(range(hyp_ep.n_regions))
-    E = (0.5 * np.ones((1,hyp_ep.n_regions))).tolist()
+    iE = np.array([50])
+    E = np.array([0.5], dtype=np.float32).tolist()
     hyp_ep.configure_e_hypothesis(iE, E, seizure_indices)
     #logger.debug(str(hyp_ep))
-    # plot_hypothesis(hyp_ep, head.connectivity.region_labels, save_flag=SAVE_FLAG, show_flag=SHOW_FLAG,
-    #                figure_dir=FOLDER_FIGURES, figsize=VERY_LARGE_SIZE)
-    #
-    # write_hypothesis(hyp_ep, folder_name=FOLDER_RES, file_name="hyp_ep.h5", hypo_name=None)
+    plot_hypothesis(hyp_ep, head.connectivity.region_labels, save_flag=SAVE_FLAG, show_flag=SHOW_FLAG,
+                   figure_dir=FOLDER_FIGURES, figsize=VERY_LARGE_SIZE)
+    write_h5_model(hyp_ep.prepare_for_h5(), folder_name=FOLDER_RES, file_name="hyp_ep.h5")
 
-    # # Test write, read and assert functions
-    # hyp_ep2 = read_hypothesis(path=os.path.join(FOLDER_RES, "hyp_ep.h5"), output="object",
-    #                           update_hypothesis=True, hypo_name=None)
-    # from tvb_epilepsy.base.utils import assert_equal_objects
-    # from tvb_epilepsy.custom.read_write import hyp_attributes_dict
-    # assert_equal_objects(hyp_ep, hyp_ep2, hyp_attributes_dict)
-
-    # hyp_exc = Hypothesis(head.number_of_regions, head.connectivity.normalized_weights,
-    #                     "x0 Hypothesis", x1eq_mode="optimize")  #"optimize" or "linTaylor"
-    # hyp_exc.K = 0.1 * hyp_exc.K
-    # ix0 =range(head.number_of_regions)
-    # x0 = (X0_DEF * np.ones((len(ix0),), dtype='float32')).tolist()
-
-    hyp_exc = Hypothesis(head.number_of_regions, head.connectivity.normalized_weights, \
-                        "EP & x0 Hypothesis", x1eq_mode="optimize")  #"linTaylor"
-    iE = np.array(range(hyp_ep.n_regions))
-    E = (0.5 * np.ones((1, hyp_ep.n_regions))).tolist()
-    hyp_exc.configure_e_hypothesis(iE, E, seizure_indices)
-    ix0 = [51]
-    x0 = (0.5 * np.ones((len(ix0),), dtype='float32')).tolist()
-
+    hyp_exc = Hypothesis(head.number_of_regions, head.connectivity.normalized_weights, "x0 Hypothesis",
+                         x1eq_mode="optimize")
+    ix0 = range(head.number_of_regions)
+    x0 = (X0_DEF * np.ones((len(ix0),), dtype='float32')).tolist()
+    x0[51] = 0.5
+    seizure_indices = np.array([51], dtype=np.int32)
     hyp_exc.configure_x0_hypothesis(ix0, x0, seizure_indices)
-
+    # logger.debug(str(hyp_exc))
     plot_hypothesis(hyp_exc, head.connectivity.region_labels,
                     save_flag=SAVE_FLAG, show_flag=SHOW_FLAG,
                     figure_dir=FOLDER_FIGURES, figsize=VERY_LARGE_SIZE)
-    h5_model = hyp_exc.prepare_for_h5()
-    write_h5_model(h5_model, folder_name=FOLDER_RES, file_name="hyp_exc.h5")
-    #
-    # x0_opt = np.array(hyp_exc.x0)
-    # x1EQ_opt = np.array(hyp_exc.x1EQ)
-    # E_opt = np.array(hyp_exc.E)
+    write_h5_model(hyp_exc.prepare_for_h5(), folder_name=FOLDER_RES, file_name="hyp_exc.h5")
 
-    # hyp_exc = cp.deepcopy(hyp_ep)
-    # hyp_exc.name = "EP & x0 Hypothesis"
-    # hyp_exc.x1eq_mode = "optimize"
-    # ix0 = [51]
-    # x0 = (0.5 * np.ones((len(ix0),), dtype='float32')).tolist()
-    #
-    # hyp_exc.configure_x0_hypothesis(ix0, x0, seizure_indices)
-    #
-    # logger.debug(str(hyp_exc))
-    # plot_hypothesis(hyp_exc, head.connectivity.region_labels,
-    #                 save_flag=SAVE_FLAG, show_flag=SHOW_FLAG,
-    #                 figure_dir=FOLDER_FIGURES, figsize=VERY_LARGE_SIZE)
-    # write_hypothesis(hyp_exc, folder_name=FOLDER_RES, file_name="hyp_exc2.h5", hypo_name=None)
+    hyp_ep_exc = Hypothesis(head.number_of_regions, head.connectivity.normalized_weights, "EP & x0 Hypothesis",
+                            x1eq_mode="optimize")
+    iE = np.array([50])
+    E = np.array([0.5], dtype=np.float32).tolist()
+    # or create the new hypothesis as a deep copy of the previous one:
+    # hyp_ep_exc = cp.deepcopy(hyp_ep)
+    # hyp_ep_exc.name = "EP & x0 Hypothesis"
+    # seizure_indices = np.array([50], dtype=np.int32)
+
+    #configure first the EP, and then the x0 hypothesis,
+    # hyp_ep_exc.configure_e_hypothesis(iE, E, seizure_indices)
+    ix0 = [51]
+    x0 = (0.5 * np.ones((len(ix0),), dtype='float32')).tolist()
+    seizure_indices = np.array([50, 51], dtype=np.int32)
+    # hyp_ep_exc.configure_x0_hypothesis(ix0, x0, seizure_indices)
+
+    # or configure them with one line:
+    hyp_ep_exc.configure_hypothesis(iE, E, ix0, x0, seizure_indices)
+
+    #logger.debug(str(hyp_ep_exc))
+    plot_hypothesis(hyp_ep_exc, head.connectivity.region_labels,
+                    save_flag=SAVE_FLAG, show_flag=SHOW_FLAG,
+                    figure_dir=FOLDER_FIGURES, figsize=VERY_LARGE_SIZE)
+    write_h5_model(hyp_ep_exc.prepare_for_h5(), folder_name=FOLDER_RES, file_name="hyp_ep_exc.h5")
 
 #------------------------------Simulation--------------------------------------
 
@@ -214,18 +201,15 @@ if __name__ == "__main__":
                                                                    scale_time=2.0, scale_fsavg=2.0,
                                                                    hpf_low=None, hpf_high=None,
                                                                    report_every_n_monitor_steps=10.0)
+    # Choose the model and build it on top of the specific hypothesis, adjust parameters:
+    if SIMULATION_MODE == "custom":
+        model_name = 'CustomEpileptor'
+    else:
+        model_name = 'EpileptorDP'  # 'EpileptorDP2D', 'EpileptorDPrealistic', "Epileptor"
 
     #Now simulate and plot for each hypothesis
     hpf_flag = False #Flag to compute and plot high pass filtered SEEG
-    for hyp in (hyp_exc, hyp_ep): # ,hyp_exc #length=30000
-
-        # Choose the model and build it on top of the specific hypothesis, adjust parameters:
-        if SIMULATION_MODE == "custom":
-            model_name = 'CustomEpileptor'
-        else:
-            model_name = 'EpileptorDP'  # 'EpileptorDP2D', 'EpileptorDPrealistic', "Epileptor"
-
-
+    for hyp in (hyp_ep, hyp_exc, hyp_ep_exc):
 
         #Launch simulation
         if SIMULATION_MODE == "custom":
@@ -252,7 +236,7 @@ if __name__ == "__main__":
                                                                         monitors_instance=None, variables_names=None)
 
             simTVB, sim_settings = simulator_instance.config_simulation(settings=sim_settings)
-            print "Initial conditions at equilibrium point: ", np.squeeze(simTVB.initial_conditions)
+            #print "Initial conditions at equilibrium point: ", np.squeeze(simTVB.initial_conditions)
             ttavg, tavg_data, status = simulator_instance.launch_simulation(n_report_blocks=n_report_blocks)
             if not(status):
                 warnings.warn("Simulation failed!")
@@ -271,24 +255,12 @@ if __name__ == "__main__":
             simulation_h5_model = simulator_instance.prepare_for_h5(sim_settings)
             write_h5_model(simulation_h5_model, folder_name=FOLDER_RES, file_name=hyp.name+"_sim_settings.h5")
 
-            # Test write, read and assert functions
-            # from tvb_epilepsy.base.utils import assert_equal_objects
-            # model2, sim_settings2 = read_simulation_settings(path=os.path.join(FOLDER_RES, hyp.name+"sim_settings.h5"),
-            #                                                  output="object", hypothesis=hyp)
-            #
-            # from tvb_epilepsy.custom.read_write import epileptor_model_attributes_dict, simulation_settings_attributes_dict
-            # assert_equal_objects(model, model2, epileptor_model_attributes_dict[model2._ui_name])
-            # #assert_equal_objects(model, model2, epileptor_model_attributes_dict[model2["_ui_name"]])
-            # assert_equal_objects(sim_settings, sim_settings2, simulation_settings_attributes_dict)
-
             #Pack results into a dictionary, high pass filter, and compute SEEG
             res = dict()
             time = scale_time * np.array(ttavg, dtype='float32')
             dt = np.min(np.diff(time))
             for iv in range(len(vois)):
                 res[vois[iv]] = np.array(tavg_data[:, iv, :], dtype='float32')
-
-            #write_ts(res, dt, path=os.path.join(FOLDER_RES, hyp.name + "_ts.h5"))
 
             if model._ui_name == "EpileptorDP2D":
                 raw_data = np.dstack([res["x1"], res["z"], res["x1"]])
@@ -321,34 +293,13 @@ if __name__ == "__main__":
             #Plot results
             plot_nullclines_eq(hyp, head.connectivity.region_labels, special_idx=hyp.seizure_indices,
                                model=str(model.nvar) + "d", zmode=model.zmode,
-                               figure_name="Hypothesis " + hyp.name + " in model " + model._ui_name +
-                                           "\nNullclines and equilibria", save_flag=SAVE_FLAG,
+                               figure_name="Nullclines and equilibria", save_flag=SAVE_FLAG,
                                show_flag=SHOW_FLAG, figure_dir=FOLDER_FIGURES)
             plot_sim_results(model, hyp, head, res, sensorsSEEG, hpf_flag)
 
             #Save results
             res['time_units'] = 'msec'
             savemat(os.path.join(FOLDER_RES, hyp.name + "_ts.mat"), res)
-            #write_object_to_h5_file(res, os.path.join(FOLDER_RES, hyp.name + "_ts.h5"),
-                                    # keys={"date": "EPI_Last_update", "version": "EPI_Version",
-                                    #       "EPI_Type": "TimeSeries"})
-
-            # from tvb_epilepsy.base.utils import read_object_from_h5_file, assert_equal_objects
-            # res2 = read_object_from_h5_file(dict(), os.path.join(FOLDER_RES, hyp.name + "_ts.h5"),
-            #                                 attributes_dict=None, add_overwrite_fields_dict=None)
-            # assert_equal_objects(res, res2)
-
-            # TODO: find out what object is that distorts subsequent simulations after the first one...
-            if hyp.name == hyp_ep.name:
-                try:
-                    assert_equal_objects(hyp, hyp_ep, attributes_dict=None)
-                except:
-                    print "Why?"
-            else:
-                try:
-                    assert_equal_objects(hyp, hyp_exc, attributes_dict=None)
-                except:
-                    print "Why?"
 
             del hyp, model, sim_settings, simulator_instance, res
 
