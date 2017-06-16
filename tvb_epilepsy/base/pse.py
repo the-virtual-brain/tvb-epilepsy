eae2268d6b157b631413cda6f380fd1a275f187b
dionperd <dionperd@gmail.com>
2017-06-16 17:19:45 +0200

    Updated pse to the refactored state of the module. Still no testing.

--- tvb_epilepsy/base/pse.py
+++ tvb_epilepsy/base/pse.py
@@ -5,11 +5,16 @@ Mechanism for parameter search exploration for LSA and simulations (it will have
 import subprocess
 import warnings
 import numpy
-from tvb_epilepsy.base.constants import EIGENVECTORS_NUMBER_SELECTION
+from copy import deepcopy
+from tvb_epilepsy.base.constants import EIGENVECTORS_NUMBER_SELECTION, K_DEF, YC_DEF, I_EXT1_DEF, A_DEF, B_DEF
 from tvb_epilepsy.base.simulators import ABCSimulator
-from tvb_epilepsy.base.hypothesis import Hypothesis
-# from tvb_epilepsy.tvb_api.simulator_tvb import SimulatorTVB
-# from tvb_epilepsy.custom.simulator_custom import SimulatorCustom
+from tvb_epilepsy.base.disease_hypothesis import DiseaseHypothesis
+from tvb_epilepsy.base.model_configuration import ModelConfiguration
+from tvb_epilepsy.base.model_configuration_service import ModelConfigurationService
+from tvb_epilepsy.base.lsa_service import LSAService
+from tvb_epilepsy.base.epileptor_model_factory import model_build_dict
+from tvb_epilepsy.tvb_api.simulator_tvb import SimulatorTVB
+from tvb_epilepsy.custom.simulator_custom import SimulatorCustom, custom_model_builder
 from tvb_epilepsy.custom.read_write import read_ts
 
 
@@ -26,41 +31,102 @@ def set_object_attribute_recursively(object, name, values, indexes):
     else:
         # ...else, set the parameter values for the specified indexes
         temp = getattr(object, name[0])
-        temp[indexes] = values #index has to be linear... i.e., 1D...
+        if len(indexes) > 0:
+            temp[indexes] = values #index has to be linear... i.e., 1D...
+        else:
+            temp = values
         setattr(object, name[0], temp)
 
 
-def hypo_out_fun(hypothesis, **kwargs):
+def pop_object_parameters(object_type, param_names, param_values, param_indexes):
+
+    object_param_names = []
+    object_param_values = []
+    object_param_indexes = []
+
+    for ip in range(len(param_names)):
+
+        if param_names[ip].split(".")[0] == object_type:
+            object_param_names.append(param_names.pop(ip).split(".")[1])
+            object_param_values.append(param_values.pop(ip))
+            object_param_indexes.append(param_indexes.pop(ip))
+
+    return object_param_names, object_param_values, object_param_indexes, param_names, param_values, param_indexes
+
+
+def update_object(object, object_type, param_names, param_values, param_indexes):
+    update_flag = False
+    object_param_names, object_param_values, object_param_indexes, param_names, param_values, param_indexes = \
+        pop_object_parameters(object_type, param_names, param_values, param_indexes)
+    for ip in range(len(object_param_names)):
+        set_object_attribute_recursively(object, object_param_names[ip], object_param_values[ip],
+                                         object_param_indexes[ip])
+        update_flag = True
+    return object, param_names, param_values, param_indexes, update_flag
+
+
+def update_hypothesis(hypothesis_input, param_names, param_values, param_indexes,
+                      model_configuration_service_input=None,
+                      yc=YC_DEF, Iext1=I_EXT1_DEF, K=K_DEF, a=A_DEF, b=B_DEF, x1eq_mode="optimize"):
+
+    # Assign possible hypothesis parameters on a new hypothesis object:
+    hypothesis = deepcopy(hypothesis_input)
+    hypothesis, param_names, param_values, param_indexes = \
+        update_object(hypothesis, "hypothesis", param_names, param_values, param_indexes)[:4]
+
+    # ...create/update a model configuration service:
+    if isinstance(model_configuration_service_input, ModelConfigurationService):
+        model_configuration_service = deepcopy(model_configuration_service_input)
+    else:
+        model_configuration_service = ModelConfigurationService(yc=yc, Iext1=Iext1, K=K, a=a, b=b, x1eq_mode=x1eq_mode)
+
+    # ...modify possible related parameters:
+    model_configuration_service, param_names, param_values, param_indexes = \
+        update_object(model_configuration_service, "model_configuration_service", param_names, param_values,
+                      param_indexes)[:4]
+
+    # ...and compute a new model_configuration:
+    if hypothesis.type == "Epileptogenicity":
+        model_configuration = model_configuration_service.configure_model_from_E_hypothesis(hypothesis)
+    else:
+        model_configuration = model_configuration_service.configure_model_from_hypothesis(hypothesis)
+
+    return hypothesis, model_configuration, param_names, param_values, param_indexes
+
+
+def lsa_out_fun(hypothesis, **kwargs):
     return hypothesis.lsa_ps
 
 
-def hypo_run_fun(hypothesis, param_names, param_values, param_indexes, out_fun=hypo_out_fun, seizure_indices=[],
-                 n_eigenvectors=EIGENVECTORS_NUMBER_SELECTION):
+def lsa_run_fun(hypothesis_input, param_names, param_values, param_indexes, out_fun=lsa_out_fun,
+                model_configuration_service_input=None,
+                yc=YC_DEF, Iext1=I_EXT1_DEF, K=K_DEF, a=A_DEF, b=B_DEF, x1eq_mode="optimize",
+                lsa_service_input=None,
+                n_eigenvectors=EIGENVECTORS_NUMBER_SELECTION, weighted_eigenvector_sum=True):
 
     try:
-        iE = []
-        E = []
-        ix0 = []
-        x0 = []
-        for ip in range(len(param_names)):
+        # Update hypothesis and create a new model_configuration:
+        hypothesis, model_configuration, param_names, param_values, param_indexes\
+            = update_hypothesis(hypothesis_input, param_names, param_values, param_indexes,
+                                model_configuration_service_input, yc, Iext1, K, a, b, x1eq_mode)
 
-            if param_names[ip] is "E":
-                iE = param_indexes[ip]
-                E = param_values[ip]
+        # ...create/update lsa service:
+        if isinstance(lsa_service_input, LSAService):
+            lsa_service = deepcopy(lsa_service_input)
+        else:
+            lsa_service = LSAService(n_eigenvectors=n_eigenvectors, weighted_eigenvector_sum=weighted_eigenvector_sum)
 
-            elif param_names[ip] is "x0":
-                ix0 = param_indexes[ip]
-                x0 = param_values[ip]
+        # ...and modify possible related parameters:
+        lsa_service = \
+            update_object(lsa_service, "lsa_service", param_names, param_values, param_indexes)[0]
 
-            else:
-                set_object_attribute_recursively(hypothesis, param_names[ip], param_values[ip], param_indexes[ip])
-
-        hypothesis.configure_hypothesis(iE, E, ix0, x0, seizure_indices, n_eigenvectors)
+        # Run LSA:
+        lsa_hypothesis = lsa_service.run_lsa(hypothesis, model_configuration)
 
         if callable(hypothesis):
-            output = out_fun(hypothesis)
+            output = out_fun(lsa_hypothesis)
         else:
-            output = hypothesis
+            output = lsa_hypothesis
 
         return True, output
 
@@ -77,50 +143,35 @@ def sim_out_fun(simulator, time, data, **kwargs):
     return {"time": time, "data": data}
 
 
-def sim_run_fun(simulator, param_names, param_values, param_indexes, out_fun=sim_out_fun, hypothesis_update=True,
-                model_update=True, initial_conditions_update=True, seizure_indices=[],
-                n_eigenvectors=EIGENVECTORS_NUMBER_SELECTION):
+def sim_run_fun(simulator_input, param_names, param_values, param_indexes, out_fun=sim_out_fun, hypothesis_input=None,
+                model_configuration_service_input=None,
+                yc=YC_DEF, Iext1=I_EXT1_DEF, K=K_DEF, a=A_DEF, b=B_DEF, x1eq_mode="optimize",
+                update_initial_conditions=True):
+
+    # Create new objects from the input simulator
+    simulator = deepcopy(simulator_input)
+    model_configuration = deepcopy(simulator_input.model_configuration)
+    model = deepcopy(simulator_input.model)
 
     try:
 
-        # First, update hypothesis and regenerate the model accordingly...
-        if hypothesis_update:
+        # First try to update model_configuration via an input hypothesis...:
+        if isinstance(hypothesis_input, DiseaseHypothesis):
+            hypothesis, model_configuration, param_names, param_values, param_indexes = \
+                update_hypothesis(hypothesis_input, param_names, param_values, param_indexes,
+                                  model_configuration_service_input, yc, Iext1, K, a, b, x1eq_mode)
+            # Update model configuration:
+            simulator.model_configuration = model_configuration
+            # ...in which case a model has to be regenerated:
+            if isinstance(simulator, SimulatorTVB):
+                model = model_build_dict[model._ui_name](model_configuration, zmode=model.zmode)
+            else:
+                model = custom_model_builder(model_configuration)
 
-            hypo_param_names = []
-            hypo_param_values = []
-            hypo_param_indexes = []
-
-            for ip in range(len(param_names)):
-
-                if param_names[ip].split(".")[0] == "hypothesis":
-
-                    hypo_param_names.append(param_names.pop(ip).split(".")[1])
-                    hypo_param_values.append(param_values.pop(ip))
-                    hypo_param_indexes.append(param_indexes.pop(ip))
-
-            if len(hypo_param_names) > 0:
-                simulator.hypothesis = hypo_run_fun(simulator.hypothesis, hypo_param_names, hypo_param_values,
-                                                    hypo_param_indexes, None, seizure_indices, n_eigenvectors)
-                simulator.configure_model()
-
-        # Then, update the model...
-        if model_update:
-
-            model_param_names = []
-            model_param_values = []
-            model_param_indexes = []
-
-            for ip in range(len(param_names)):
-
-                if param_names[ip].split(".")[0] == "model":
-
-                    model_param_names.append(param_names.pop(ip).split(".")[1])
-                    model_param_values.append(param_values.pop(ip))
-                    model_param_indexes.append(param_indexes.pop(ip))
-
-            if len(model_param_names) > 0:
-                set_object_attribute_recursively(simulator.model, model_param_names, model_param_values,
-                                                  model_param_indexes)
+        # Now (further) update model if needed:
+        model, param_names, param_values, param_indexes = \
+            update_object(model, "model", param_names, param_values, param_indexes)
+        simulator.model = model
 
         # Now, update other possible remaining parameters, i.e., concerning the integrator, noise etc...
         for ip in range(len(param_names)):
@@ -128,7 +179,7 @@ def sim_run_fun(simulator, param_names, param_values, param_indexes, out_fun=sim
 
         # Now, recalculate the default initial conditions...
         # If initial conditions were parameters, then, this flag can be set to False
-        if initial_conditions_update:
+        if update_initial_conditions:
             simulator.configure_initial_conditions()
 
         time, data, status = simulator.launch()
@@ -158,14 +209,14 @@ class PSE(object):
         if task is "LSA":
 
             # TODO: this will not work anymore
-            if isinstance(hypothesis, Hypothesis):
+            if isinstance(hypothesis, DiseaseHypothesis):
                 self.pse_object = hypothesis
 
             else:
                 raise ValueError("\ntask = LSA" + str(task) + " but hypothesis is not a Hypothesis object!")
 
-            def_run_fun = hypo_run_fun
-            def_out_fun = hypo_out_fun
+            def_run_fun = lsa_run_fun
+            def_out_fun = lsa_out_fun
 
         else:
 
