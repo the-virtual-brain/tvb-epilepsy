0e265aa6f39218214393f375511891b2df7a5da3
dionperd <dionperd@gmail.com>
2017-09-27 13:22:37 +0200

    Corrected aalpha to alpha...

--- tvb_epilepsy/service/sampling_service.py
+++ tvb_epilepsy/service/sampling_service.py
@@ -96,7 +96,7 @@ def gamma_from_mu_std(mu, std):
 
 def gamma_to_mu_std(p):
     if p.get("alpha", False) and p.get("beta", False):
-        return p["alpha"] / p["beta"], np.sqrt(p["aalpha"]) / p["beta"]
+        return p["alpha"] / p["beta"], np.sqrt(p["alpha"]) / p["beta"]
 
     elif p.get("k", False) and p.get("theta", False):
         return p["k"] * p["theta"], np.sqrt(p["k"]) * p["theta"]
