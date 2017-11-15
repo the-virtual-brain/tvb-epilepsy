f11e8e4de2d73ab47d08fb56d6d2ed8ba60d1302
dionperd <dionperd@gmail.com>
2017-11-15 16:33:05 +0100

    Created StochasticParameterBase and adjusted __str__

--- tvb_epilepsy/base/model/statistical_models/stochastic_parameter.py
+++ tvb_epilepsy/base/model/statistical_models/stochastic_parameter.py
@@ -1,60 +1,57 @@
 import importlib
 
+from abc import ABCMeta
+
+
 from tvb_epilepsy.base.constants import MAX_SINGLE_VALUE
 from tvb_epilepsy.base.utils.data_structures_utils import formal_repr, sort_dict
 from tvb_epilepsy.base.model.parameter import Parameter
 from tvb_epilepsy.base.model.statistical_models.probability_distributions.probability_distribution import \
-                                                                                                     compute_pdf_params
+                                                    ProbabilityDistribution, compute_pdf_params, generate_distribution
 
 
-def generate_stochastic_parameter(name="Parameter", low=-MAX_SINGLE_VALUE, high=MAX_SINGLE_VALUE, shape=(1,),
+class StochasticParameterBase(Parameter, ProbabilityDistribution):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, name="Parameter", low=-MAX_SINGLE_VALUE, high=MAX_SINGLE_VALUE, p_shape=(1,), **pdf_params):
+        Parameter.__init__(self, name, low, high, p_shape)
+
+    def __repr__(self):
+        d = {"01. name": self.name,
+             "02. low": self.low,
+             "03. high": self.high,
+             }
+        return formal_repr(self, sort_dict(d))
+
+    def __str__(self):
+        return self.__repr__()
+
+    def _prepare_for_h5(self):
+        return Parameter._prepare_for_h5(self)
+
+    def write_to_h5(self, folder, filename=""):
+        Parameter.write_to_h5(self)
+
+
+def generate_stochastic_parameter(name="Parameter", low=-MAX_SINGLE_VALUE, high=MAX_SINGLE_VALUE, p_shape=(1,),
                                   probability_distribution="uniform", optimize=False, **pdf_params):
     pdf_module = importlib.import_module("tvb_epilepsy.base.model.statistical_models.probability_distributions." +
                                   probability_distribution.lower() + "_distribution")
-    ProbabilityDistribution = eval("pdf_module." + probability_distribution.title() + "Distribution")
+    thisProbabilityDistribution = eval("pdf_module." + probability_distribution.title() + "Distribution")
     if optimize:
         pdf_params = compute_pdf_params(probability_distribution.lower(), pdf_params)
 
-    class StochasticParameter(Parameter, ProbabilityDistribution):
-
+    class StochasticParameter(StochasticParameterBase, thisProbabilityDistribution):
         def __init__(self, name="Parameter", low=-MAX_SINGLE_VALUE, high=MAX_SINGLE_VALUE, p_shape=(1,), **pdf_params):
-            Parameter.__init__(self, name, low, high, p_shape)
-            ProbabilityDistribution.__init__(self, **pdf_params)
-
-        def __repr__(self):
-            d = {"1. type": self.name,
-                 "2. low": self.low,
-                 "3. high": self.high,
-                 "4. shape": self.p_shape,
-                 "5. distribution": self.type,
-                 "6. pdf_params": self.pdf_params(),
-                 "7. n_params": self.n_params,
-                 "8. constraint": self.constraint_string,
-                 "9. shape": self.p_shape,
-                 "10. mean": self.mean,
-                 "11. median": self.median,
-                 "12. mode": self.mode,
-                 "13. var": self.var,
-                 "14. std": self.std,
-                 "15. skew": self.skew,
-                 "16. kurt": self.kurt,
-                 "17. scipy_name": self.scipy_name,
-                 "18. numpy_name": self.numpy_name
-                 }
-            return formal_repr(self, sort_dict(d))
-
+            StochasticParameterBase.__init__(self, name, low, high, p_shape)
+            thisProbabilityDistribution.__init__(self, **pdf_params)
         def __str__(self):
-            return self.__repr__()
 
-        def _prepare_for_h5(self):
-            return Parameter._prepare_for_h5(self)
-
-        def write_to_h5(self, folder, filename=""):
-            Parameter.write_to_h5(self)
-
-    return StochasticParameter(name, low, high, shape, **pdf_params)
+            return StochasticParameterBase.__str__(self) + "\n" \
+                   + "\n".join(thisProbabilityDistribution.__str__(self).splitlines()[1:])
+    return StochasticParameter(name, low, high, p_shape, **pdf_params)
 
 
 if __name__ == "__main__":
-    sp = generate_stochastic_parameter("test", probability_distribution="normal", optimize=True, mean=1.0, std=2.0)
+    sp = generate_stochastic_parameter("test", probability_distribution="gamma", optimize=False, shape=1.0, scale=2.0)
     print(sp)
