eafa8203de9e00ba99207ac39d7bb4883c436033
dionperd <dionperd@gmail.com>
2017-11-15 16:31:21 +0100

    Removed abstractmethods not used

--- tvb_epilepsy/base/model/statistical_models/probability_distributions/discrete_probability_distribution.py
+++ tvb_epilepsy/base/model/statistical_models/probability_distributions/discrete_probability_distribution.py
@@ -16,39 +16,3 @@ class DiscreteProbabilityDistribution(ProbabilityDistribution):
             x = np.linspace(np.min(self.scipy(loc, scale).ppf(q[0])),
                             np.min(self.scipy(loc, scale).ppf(q[1])), 101)
         return self.scipy(loc, scale).pmf(x), x
-
-    @abstractmethod
-    def constraint(self):
-        pass
-
-    @abstractmethod
-    def scipy(self, loc=0.0, scale=1.0):
-        pass
-
-    @abstractmethod
-    def calc_mean(self, use="scipy"):
-        pass
-
-    @abstractmethod
-    def calc_median(self, use="scipy"):
-        pass
-
-    @abstractmethod
-    def calc_mode(self):
-        pass
-
-    @abstractmethod
-    def calc_var(self, use="scipy"):
-        pass
-
-    @abstractmethod
-    def calc_std(self, use="scipy"):
-        pass
-
-    @abstractmethod
-    def calc_skew(self, use="scipy"):
-        pass
-
-    @abstractmethod
-    def calc_kurt(self, use="scipy"):
-        pass
