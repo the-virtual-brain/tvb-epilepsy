888dc07a25a385a442368c392358000c458282a0
dionperd <dionperd@gmail.com>
2017-12-15 18:28:36 +0100

    Functions to return sensors from sensors/electrode labels Changing channels ->  electrods, contacts -> sensors

--- tvb_epilepsy/base/model/vep/sensors.py
+++ tvb_epilepsy/base/model/vep/sensors.py
@@ -1,5 +1,6 @@
 
 from collections import OrderedDict
+import re
 
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
@@ -9,7 +10,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from tvb_epilepsy.base.constants.configurations import FOLDER_FIGURES, VERY_LARGE_SIZE, FIG_FORMAT, SAVE_FLAG, SHOW_FLAG
 from tvb_epilepsy.base.utils.log_error_utils import raise_value_error, warning
 from tvb_epilepsy.base.utils.data_structures_utils import reg_dict, formal_repr, sort_dict, ensure_list, \
-                                                                       split_string_text_numbers, construct_import_path
+                                                        labels_to_inds, split_string_text_numbers, construct_import_path
 from tvb_epilepsy.base.utils.math_utils import compute_gain_matrix, select_greater_values_array_inds
 from tvb_epilepsy.base.utils.plot_utils import save_figure, check_show
 from tvb_epilepsy.base.h5_model import convert_to_h5_model
@@ -38,11 +39,14 @@ class Sensors(object):
         self.orientations = orientations
         self.gain_matrix = gain_matrix
         self.s_type = s_type
+        self.elec_labels = np.array([])
+        self.elec_inds = np.array([])
         if len(self.labels) > 1:
+            self.elec_labels, self.elec_inds = self.group_sensors_to_electrodes()
             if self.needles.size == self.number_of_sensors:
                 self.channel_labels, self.channel_inds = self.get_inds_labels_from_needles()
             else:
-                self.channel_labels, self.channel_inds = self.group_contacts_to_electrodes()
+                self.channel_labels, self.channel_inds = self.group_sensors_to_electrodes()
                 self.get_needles_from_inds_labels()
         self.context_str = "from " + construct_import_path(__file__) + " import Sensors"
         self.create_str = "Sensors(np.array([]), np.array([]), s_type='" + self.s_type + "')"
@@ -85,7 +89,7 @@ class Sensors(object):
         h5_model = self._prepare_for_h5()
         h5_model.write_to_h5(folder, filename)
 
-    def contact_label_to_index(self, labels):
+    def sensor_label_to_index(self, labels):
         indexes = []
         for label in labels:
             try:
@@ -97,6 +101,21 @@ class Sensors(object):
         else:
             return indexes
 
+    def get_sensors_inds_by_sensors_labels(self, lbls):
+        return labels_to_inds(self.labels, lbls)
+
+    def get_elecs_inds_by_elecs_labels(self, lbls):
+        return labels_to_inds(self.elec_labels, lbls)
+
+    def get_sensors_inds_by_elec_labels(self, lbls):
+        elec_inds = self.get_elecs_inds_by_elecs_labels(lbls)
+        sensors_inds = []
+        for ind in elec_inds:
+            sensors_inds += self.elec_inds[ind]
+        return np.unique(sensors_inds)
+
+    def calculate_projection(self, connectivity):
+        return compute_gain_matrix(self.locations, connectivity.centers, normalize=95, ceil=1.0)
     def compute_gain_matrix(self, connectivity):
         return compute_gain_matrix(self.locations, connectivity.centres, normalize=95, ceil=1.0)
 
@@ -115,17 +134,45 @@ class Sensors(object):
         for id, inds in enumerate(self.channel_inds):
             self.needles[inds] = id
 
-    def group_contacts_to_electrodes(self, labels=None):
+    def group_sensors_to_electrodes(self, labels=None):
         if labels is None:
             labels = self.labels
-        contact_names = np.array(split_string_text_numbers(labels))
-        channel_labels = np.unique(contact_names[:, 0])
-        channel_inds = []
-        for chlbl in channel_labels:
-            channel_inds.append(np.where(contact_names[:, 0] == chlbl)[0])
-        return channel_labels, channel_inds
+        sensor_names = np.array(split_string_text_numbers(labels))
+        elec_labels = np.unique(sensor_names[:, 0])
+        elec_inds = []
+        for chlbl in elec_labels:
+            elec_inds.append(np.where(sensor_names[:, 0] == chlbl)[0])
+        return elec_labels, elec_inds
 
-    def select_contacts_rois(self, rois=None, initial_selection=[], gain_matrix_th=0.5):
+    def get_bipolar_sensors(self, sensors_inds=None):
+        if sensors_inds is None:
+            sensors_inds = range(self.number_of_sensors)
+        bipolar_sensors_lbls = []
+        bipolar_sensors_inds = []
+        for ind in range(len(sensors_inds) - 1):
+            iS1 = sensors_inds[ind]
+            iS2 = sensors_inds[ind+1]
+            if (self.labels[iS1][0] == self.labels[iS2 + 1][0]) and \
+                    int(re.findall(r'\d+', self.labels[iS1])) == \
+                    int(re.findall(r'\d+', self.labels[iS2 + 1]) - 1):
+                bipolar_sensors_lbls.append(self.labels[iS1] + "-" + self.labels[iS2])
+                bipolar_sensors_inds.append(iS1)
+        return bipolar_sensors_inds, bipolar_sensors_lbls
+
+    def get_bipolar_elecs(self, elecs):
+        try:
+            bipolar_sensors_lbls = []
+            bipolar_sensors_inds = []
+            for elec_ind in elecs:
+                curr_inds, curr_lbls = self.get_bipolar_sensors(sensors_inds=self.elec_inds[elec_ind])
+                bipolar_sensors_inds.append(curr_inds)
+                bipolar_sensors_lbls.append(curr_lbls)
+        except:
+            elecs_inds = self.get_elecs_inds_by_elecs_labels(elecs)
+            bipolar_sensors_inds, bipolar_sensors_lbls = self.get_bipolar_elecs(elecs_inds)
+        return bipolar_sensors_inds, bipolar_sensors_lbls
+
+    def select_sensors_rois(self, rois=None, initial_selection=[], projection_th=0.5):
         if len(initial_selection) == 0:
             initial_selection = range(self.number_of_sensors)
         selection = []
@@ -136,41 +183,41 @@ class Sensors(object):
                 selection += (np.array(initial_selection)[select_greater_values_array_inds(proj, gain_matrix_th)]).tolist()
         return np.unique(selection).tolist()
 
-    def select_contacts_power(self, power, selection=[], power_th=0.5):
+    def select_sensors_power(self, power, selection=[], power_th=0.5):
         if len(selection) == 0:
             selection = range(self.number_of_sensors)
         return (np.array(selection)[select_greater_values_array_inds(power, power_th)]).tolist()
 
-    def select_contacts_corr(self, distance, initial_selection=[], n_electrodes=10, contacts_per_electrode=1,
+    def select_sensors_corr(self, distance, initial_selection=[], n_electrodes=10, sensors_per_electrode=1,
                              power=None, group_electrodes=False):
         if len(initial_selection) == 0:
             initial_selection = range(self.number_of_sensors)
-        n_contacts = len(initial_selection)
-        if n_contacts > 2:
+        n_sensors = len(initial_selection)
+        if n_sensors > 2:
             initial_selection = np.array(initial_selection)
             distance = 1.0 - distance
             if group_electrodes:
-                channel_labels, channel_inds = self.group_contacts_to_electrodes(self.labels[initial_selection])
-                if len(channel_labels) >= 2:
-                    noconnectivity = np.ones((n_contacts, n_contacts))
-                    for ch in channel_inds:
+                elec_labels, elec_inds = self.group_sensors_to_electrodes(self.labels[initial_selection])
+                if len(elec_labels) >= 2:
+                    noconnectivity = np.ones((n_sensors, n_sensors))
+                    for ch in elec_inds:
                         noconnectivity[np.meshgrid(ch, ch)] = 0.0
                     distance = distance * noconnectivity
-            n_electrodes = np.minimum(np.maximum(n_electrodes, 3), n_contacts//contacts_per_electrode)
+            n_electrodes = np.minimum(np.maximum(n_electrodes, 3), n_sensors//sensors_per_electrode)
             clustering = AgglomerativeClustering(n_electrodes, affinity="precomputed", linkage="average")
             clusters_labels = clustering.fit_predict(distance)
             selection = []
             for cluster_id in range(len(np.unique(clusters_labels))):
                 cluster_inds = np.where(clusters_labels == cluster_id)[0]
-                n_select = np.minimum(contacts_per_electrode, len(cluster_inds))
-                if power is not None and len(ensure_list(power)) == n_contacts:
+                n_select = np.minimum(sensors_per_electrode, len(cluster_inds))
+                if power is not None and len(ensure_list(power)) == n_sensors:
                     inds_select = np.argsort(power[cluster_inds])[-n_select:]
                 else:
                     inds_select = range(n_select)
                 selection.append(initial_selection[cluster_inds[inds_select]])
             return np.unique(np.hstack(selection)).tolist()
         else:
-            warning("Number of contacts' left < 6!\n" + "Skipping clustering and returning all of them!")
+            warning("Number of sensors' left < 6!\n" + "Skipping clustering and returning all of them!")
             return initial_selection
 
     def plot_gain_matrix(self, region_labels, figure=None, title="Projection", y_labels=1, x_labels=1,
