0a808ba216571347ba75a2d355bb605bce9557a7
popaula937 <paula.popa@codemart.ro>
2018-01-05 17:29:53 +0200

    Added sensors to test head.

--- tvb_epilepsy/tests/test_head_service.py
+++ tvb_epilepsy/tests/test_head_service.py
@@ -1,6 +1,8 @@
 import os
+import numpy
 from tvb_epilepsy.base.constants.configurations import FOLDER_LOGS, FOLDER_RES, FOLDER_FIGURES
 from tvb_epilepsy.base.model.vep.head import Head
+from tvb_epilepsy.base.model.vep.sensors import Sensors
 from tvb_epilepsy.base.model.vep.surface import Surface
 from tvb_epilepsy.custom.readers_custom import CustomReader
 from tvb_epilepsy.service.head_service import HeadService
@@ -20,10 +22,10 @@ class TestHeadService():
 
     def _prepare_dummy_head(self):
         reader = CustomReader()
-        # TODO: add sensors to head to increase coverage
         connectivity = reader.read_connectivity(os.path.join(data_dir, "Connectivity.h5"))
         cort_surface = Surface([], [])
-        head = Head(connectivity, cort_surface)
+        seeg_sensors = Sensors(numpy.array(["sens1", "sens2"]), numpy.array([[0, 0, 0], [0, 1, 0]]))
+        head = Head(connectivity, cort_surface, sensorsSEEG=seeg_sensors)
 
         return head
 
@@ -40,19 +42,24 @@ class TestHeadService():
 
         assert os.path.exists(head_folder)
         assert os.listdir(head_folder) is not []
+        assert len(os.listdir(head_folder)) == 2
 
     def test_plot_head(self):
         head = self._prepare_dummy_head()
+        #TODO: this filenames may change because they are composed inside the plotting functions
         filename1 = "Connectivity_.png"
         filename2 = "HeadStats.png"
+        filename3 = "1_-_SEEG_-_Projection.png"
 
         assert not os.path.exists(os.path.join(FOLDER_FIGURES, filename1))
         assert not os.path.exists(os.path.join(FOLDER_FIGURES, filename2))
+        assert not os.path.exists(os.path.join(FOLDER_FIGURES, filename3))
 
         self.head_service.plot_head(head, save_flag=True, show_flag=False, figure_dir=FOLDER_FIGURES)
 
         assert os.path.exists(os.path.join(FOLDER_FIGURES, filename1))
         assert os.path.exists(os.path.join(FOLDER_FIGURES, filename2))
+        assert os.path.exists(os.path.join(FOLDER_FIGURES, filename3))
 
     # def test_select_sensors_power(self):
 
