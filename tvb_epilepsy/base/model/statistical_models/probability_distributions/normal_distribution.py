a46de33288c877e86d54846ac58e0a2c3d5d64c8
dionperd <dionperd@gmail.com>
2018-01-15 13:14:14 +0100

    Changed parameters mean of normal distribution back to mu

--- tvb_epilepsy/base/model/statistical_models/probability_distributions/normal_distribution.py
+++ tvb_epilepsy/base/model/statistical_models/probability_distributions/normal_distribution.py
@@ -16,29 +16,29 @@ class NormalDistribution(ContinuousProbabilityDistribution):
         self.type = "normal"
         self.scipy_name = "norm"
         self.numpy_name = "normal"
-        self.mean = make_float(params.get("mean", params.get("loc", 0.0)))
+        self.mu = make_float(params.get("mu", params.get("loc", 0.0)))
         self.sigma = make_float(make_float(params.get("sigma", params.get("scale", 1.0))))
         self.constraint_string = "sigma > 0"
-        self.__update_params__(mean=self.mean, sigma=self.sigma)
+        self.__update_params__(mu=self.mu, sigma=self.sigma)
         self.context_str = "from " + construct_import_path(__file__) + " import NormalDistribution"
         self.create_str = "NormalDistribution('" + self.type + "')"
         self.update_str = "obj.update_params()"
 
-    def pdf_params(self, parametrization="mean-sigma"):
+    def pdf_params(self, parametrization="mu-sigma"):
         p = OrderedDict()
         if isequal_string(parametrization, "scipy") or isequal_string(parametrization, "numpy"):
-            p.update(zip(["loc", "scale"], [self.mean, self.sigma]))
+            p.update(zip(["loc", "scale"], [self.mu, self.sigma]))
             return p
         else:
-            p.update(zip(["mean", "sigma"], [self.mean, self.sigma]))
+            p.update(zip(["mu", "sigma"], [self.mu, self.sigma]))
             return p
 
     def scale_params(self, loc=0.0, scale=1.0):
-        return self.mean + loc, self.sigma * scale
+        return self.mu + loc, self.sigma * scale
 
     def update_params(self, loc=0.0, scale=1.0, use="scipy", **params):
         self.__update_params__(loc, scale, use,
-                               mean=make_float(params.get("mean", params.get("loc", self.mean))),
+                               mu=make_float(params.get("mu", params.get("loc", self.mu))),
                                sigma=make_float(params.get("sigma", params.get("scale", self.sigma))))
 
     def constraint(self):
@@ -46,22 +46,22 @@ class NormalDistribution(ContinuousProbabilityDistribution):
         return np.array(self.sigma).flatten() - np.finfo(np.float64).eps
 
     def scipy(self, loc=0.0, scale=1.0):
-        return getattr(ss, self.scipy_name)(loc=self.mean+loc, scale=self.sigma*scale)
+        return getattr(ss, self.scipy_name)(loc=self.mu+loc, scale=self.sigma*scale)
 
     def numpy(self, loc=0.0, scale=1.0, size=(1,)):
-        return lambda: nr.normal(self.mean + loc, self.sigma * scale, size=size)
+        return lambda: nr.normal(self.mu + loc, self.sigma * scale, size=size)
 
     def calc_mean_manual(self, loc=0.0, scale=1.0):
-        return self.mean + loc
+        return self.mu + loc
 
     def calc_median_manual(self, loc=0.0, scale=1.0):
-        return self.mean + loc
+        return self.mu + loc
 
     def calc_mode_manual(self, loc=0.0, scale=1.0):
-        return self.mean + loc
+        return self.mu + loc
 
     def calc_var_manual(self, loc=0.0, scale=1.0):
-        return self.sigma**2 * scale**2
+        return (self.sigma * scale) ** 2
 
     def calc_std_manual(self, loc=0.0, scale=1.0):
         return self.sigma * scale
