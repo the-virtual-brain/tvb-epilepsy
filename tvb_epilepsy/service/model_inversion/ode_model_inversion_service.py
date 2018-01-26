cf43483b7bd36692b40b0f9c10d177a75b19513a
dionperd <dionperd@gmail.com>
2018-01-26 19:55:52 +0100

    Added and tested plotting functions for vep models in model inversion services

--- tvb_epilepsy/service/model_inversion/ode_model_inversion_service.py
+++ tvb_epilepsy/service/model_inversion/ode_model_inversion_service.py
@@ -1,7 +1,7 @@
 import time
 from copy import deepcopy
 import numpy as np
-# import pylab as pl
+from tvb_epilepsy.base.constants.configurations import FOLDER_FIGURES, VERY_LARGE_SIZE, FIG_FORMAT, SHOW_FLAG, SAVE_FLAG
 from tvb_epilepsy.base.constants.model_constants import X1_DEF
 from tvb_epilepsy.base.constants.model_inversion_constants import X1INIT_MIN, X1INIT_MAX, ZINIT_MIN, ZINIT_MAX, \
                                                                             MC_MIN,  SIG_INIT_DEF, OBSERVATION_MODELS
@@ -322,14 +322,18 @@ class ODEModelInversionService(ModelInversionService):
         model_data["offset_signal_p"] = np.array(statistical_model.parameters["offset_signal"].pdf_params().values())
         return sort_dict(model_data)
 
-    # def violin_x0(self, csv, skip=0, x0c=-1.8, x0lim=(-6, 0), per_chain=False):
-    #     from pylab import subplot, axhline, violinplot, ylim, legend, xlabel, title
-    #     if not per_chain:
-    #         from ..io.stan import merge_csv_data
-    #         csv = [merge_csv_data(*csv, skip=skip)]
-    #     for i, csvi in enumerate(csv):
-    #         subplot(1, len(csv), i + 1)
-    #         axhline(x0c, color='r');
-    #         violinplot(csvi['x0'])
-    #         ylim(x0lim)
-    #         legend((f'x0 < {x0c} healthy', 'p(x0)',)), xlabel('Region'), title(f'Chain {i+1}')
+    def parameters_pair_plots(self, samples,
+                             params=["tau1", "tau0", "K", "sig_eq", "sig_init", "eps", "scale_signal", "offset_signal"],
+                             skip_samples=0, title='Parameters fit', figure_name=None, figure_dir=FOLDER_FIGURES,
+                           figsize=VERY_LARGE_SIZE, figure_format=FIG_FORMAT, show_flag=SHOW_FLAG, save_flag=SAVE_FLAG):
+        super(ODEModelInversionService, self).parameters_pair_plots(samples, params, skip_samples, title, figure_name,
+                                                                    figure_dir, figsize, figure_format, show_flag,
+                                                                    save_flag)
+
+    def region_parameters_violin_plots(self, samples, params=["x0", "x1eq", "x1init", "zinit"], skip_samples=0,
+                                       per_chain=False, figure_name="Regions parameters samples",
+                                       figure_dir=FOLDER_FIGURES, figsize=VERY_LARGE_SIZE, figure_format=FIG_FORMAT,
+                                       show_flag=SHOW_FLAG,  save_flag=SAVE_FLAG):
+        super(ODEModelInversionService, self).region_parameters_violin_plots(samples, params, skip_samples, per_chain,
+                                                                             figure_name, figure_dir, figsize,
+                                                                             figure_format, show_flag, save_flag)
