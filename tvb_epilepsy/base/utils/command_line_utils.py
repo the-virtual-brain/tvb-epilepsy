2d43e40f6c4f8f1e3c0539ab39b32f696cc899e1
dionperd <dionperd@gmail.com>
2018-02-01 16:38:31 +0100

    Print command in command line utils

--- tvb_epilepsy/base/utils/command_line_utils.py
+++ tvb_epilepsy/base/utils/command_line_utils.py
@@ -16,6 +16,9 @@ def execute_command(command, cwd=os.getcwd(), shell=True, logger=None):
         logger = initialize_logger(__name__)
     logger.info("Running process in directory:\n" + cwd)
     logger.info("Command:\n"+ command)
+    # TODO: make logger infor printable to the console!
+    print("Running process in directory:\n" + cwd)
+    print("Command:\n" + command)
     tic = time.time()
     process = subprocess.Popen(command, shell=shell, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                                universal_newlines=True)
@@ -31,5 +34,5 @@ def execute_command(command, cwd=os.getcwd(), shell=True, logger=None):
         return output, time.time() - tic
     else:
         logger.info("The process ran for " + str(time.time() - tic))
-        # print("The process ran for " + str(time.time() - tic))
+        print("The process ran for " + str(time.time() - tic))
         raise subprocess.CalledProcessError(exitCode, command)
