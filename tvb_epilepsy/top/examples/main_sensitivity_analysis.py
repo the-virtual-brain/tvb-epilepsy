353dab02c44858b522fd74a500b92f1df74cfc41
popaula937 <paula.popa@codemart.ro>
2018-02-12 13:02:51 +0200

    EP-813. Used HypothesisBuilder in main_sensitivity_analysis.

--- tvb_epilepsy/top/examples/main_sensitivity_analysis.py
+++ tvb_epilepsy/top/examples/main_sensitivity_analysis.py
@@ -3,9 +3,9 @@ import numpy as np
 from tvb_epilepsy.base.constants.model_constants import K_DEF
 from tvb_epilepsy.base.constants.configurations import DATA_CUSTOM, FOLDER_RES
 from tvb_epilepsy.base.utils.log_error_utils import initialize_logger
-from tvb_epilepsy.base.model.disease_hypothesis import DiseaseHypothesis
 from tvb_epilepsy.io.h5_writer import H5Writer
 from tvb_epilepsy.plot.plotter import Plotter
+from tvb_epilepsy.service.hypothesis_builder import HypothesisBuilder
 from tvb_epilepsy.service.sensitivity_analysis_service import METHODS
 from tvb_epilepsy.top.scripts.sensitivity_analysis_sripts import sensitivity_analysis_pse_from_hypothesis
 from tvb_epilepsy.io.h5_reader import H5Reader as Reader
@@ -33,10 +33,8 @@ if __name__ == "__main__":
     healthy_indices = np.delete(all_regions_indices, disease_indices).tolist()
     n_healthy = len(healthy_indices)
     # This is an example of x0_values mixed Excitability and Epileptogenicity Hypothesis:
-    hyp_x0_E = DiseaseHypothesis(head.connectivity.number_of_regions,
-                                 excitability_hypothesis={tuple(x0_indices): x0_values},
-                                 epileptogenicity_hypothesis={tuple(e_indices): e_values},
-                                 connectivity_hypothesis={})
+    hyp_x0_E = HypothesisBuilder().set_nr_of_regions(head.connectivity.number_of_regions).build_mixed_hypothesis(
+        e_values, e_indices, x0_values, x0_indices)
     # Now running the sensitivity analysis:
     logger.info("running sensitivity analysis PSE LSA...")
     for m in METHODS:
@@ -52,8 +50,8 @@ if __name__ == "__main__":
                                                              {"name": "x0_values", "indices": healthy_indices}],
                                                          logger=logger, save_services=True)
             Plotter().plot_lsa(lsa_hypothesis, model_configuration, lsa_service.weighted_eigenvector_sum,
-                         lsa_service.eigen_vectors_number, region_labels=head.connectivity.region_labels,
-                                 pse_results=pse_results, title=m + "_PSE_LSA_overview_" + lsa_hypothesis.name)
+                               lsa_service.eigen_vectors_number, region_labels=head.connectivity.region_labels,
+                               pse_results=pse_results, title=m + "_PSE_LSA_overview_" + lsa_hypothesis.name)
             # , show_flag=True, save_flag=False
             writer.write_dictionary(pse_results,
                                     os.path.join(FOLDER_RES, m + "_PSE_LSA_results_" + lsa_hypothesis.name + ".h5"))
