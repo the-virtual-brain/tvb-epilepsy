b0776f75097c47163e177ef9a7b7ca5efd3dc42a
dionperd <dionperd@gmail.com>
2018-03-13 18:24:31 +0100

    Added epileptor_params_factory

---
+++ tvb_epilepsy/service/model_inversion/epileptor_params_factory.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from tvb_epilepsy.base.computations.probability_distributions import ProbabilityDistributionTypes
+from tvb_epilepsy.service.stochastic_parameter_builder import generate_stochastic_parameter
+
+
+def generate_lognormal(name, high, low, mean, sigma=None, sigma_scale=3, p_shape=(), use="scipy"):
+    if sigma is None:
+        sigma = np.minimum(np.abs(high - mean), np.abs(mean - low)) / sigma_scale
+    return generate_stochastic_parameter(name, low, high, loc=0.0, scale=1.0, p_shape=p_shape,
+                                         probability_distribution=ProbabilityDistributionTypes.LOGNORMAL,
+                                         optimize_pdf=True, use=use, **{"mean": mean/sigma, "skew": 0.0}). \
+                                        update_loc_scale(use=use, **{"mean": mean, "std": sigma})
+
+
+def generate_negative_lognormal(name, high, low, mean, sigma=None, sigma_scale=3, p_shape=(), use="scipy"):
+    return generate_lognormal(name, high - low, 0.0, high - mean, sigma, sigma_scale, p_shape, use)
