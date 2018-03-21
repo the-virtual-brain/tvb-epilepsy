f57601d03b2977ef8067b78c65e32913a5989176
popaula937 <paula.popa@codemart.ro>
2018-03-21 18:18:45 +0200

    EP-847. Example of how to use numpy structured array with TS.

--- tvb_epilepsy/top/examples/main_signals.py
+++ tvb_epilepsy/top/examples/main_signals.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+import numpy
 from tvb_epilepsy.base.model.timeseries import Timeseries
 from tvb_epilepsy.io.h5_reader import H5Reader
 import h5py
@@ -16,6 +17,39 @@ def read_ts(path):
 
     return data, total_time, nr_of_steps, start_time
 
+def prepare_dtype_for_2D(labels_list):
+    list_of_dtype_tuples = []
+    for label in labels_list:
+        list_of_dtype_tuples.append((label, "f4"))
+
+    return list_of_dtype_tuples
+
+
+def prepare_dtype_for_3D(labels_list, sub_dimension_dtype=None, first_dimension_size=None):
+    list_of_dtype_tuples = []
+    if sub_dimension_dtype is None or first_dimension_size is None:
+        list_of_dtype_tuples = prepare_dtype_for_2D(labels_list)
+    else:
+        for label in labels_list:
+            list_of_dtype_tuples.append((label, sub_dimension_dtype, (first_dimension_size,)))
+
+    return list_of_dtype_tuples
+
+
+def prepare_data_for_structured_array(data):
+    result = []
+    if len(data.shape) == 2:
+        for vector in data:
+            result.append(tuple(vector))
+    if len(data.shape) == 3:
+        for vector in numpy.transpose(data, (1, 0, 2)):
+            subresult = []
+            for subvector in vector:
+                subresult.append(tuple(subvector))
+            result.append(subresult)
+
+    return result
+
 
 if __name__ == "__main__":
     data, total_time, nr_of_steps, start_time = read_ts("/WORK/Episense/trunk/demo-data/Head_TREC/epHH/ts.h5")
@@ -23,8 +57,18 @@ if __name__ == "__main__":
     signal = Timeseries(data, OrderedDict({"space": list(conn.region_labels), "state_variables": ["a", "b", "c"]}),
                         start_time, total_time / float(nr_of_steps))
 
-    timeline = signal.get_time_line()
-    sv = signal.get_state_variable("a")
-    subspace = signal.get_subspace_by_labels(list(conn.region_labels)[:3])
-    timewindow = signal.get_time_window(10, 100)
-    timewindowUnits = signal.get_time_window_by_units(timeline[10], timeline[100])
+    #     timeline = signal.get_time_line()
+    #     sv = signal.get_state_variable("a")
+    #     subspace = signal.get_subspace_by_labels(list(conn.region_labels)[:3])
+    #     timewindow = signal.get_time_window(10, 100)
+    #     timewindowUnits = signal.get_time_window_by_units(timeline[10], timeline[100])
+
+    sv = prepare_dtype_for_2D(["a", "b", "c"])
+    rgs = prepare_dtype_for_3D(conn.region_labels)
+    rgs_sv = prepare_dtype_for_3D(conn.region_labels, sv, nr_of_steps)
+
+    data2D = prepare_data_for_structured_array(data[0])
+    data3D = prepare_data_for_structured_array(data)
+
+    struct_data2D = numpy.array(data2D, dtype=sv)
+    struct_data3D = numpy.array(tuple(data3D), dtype=rgs_sv)
