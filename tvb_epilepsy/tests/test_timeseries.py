222cb6cb15112a378383391d9d6caaf08c2d496b
popaula937 <paula.popa@codemart.ro>
2018-03-27 15:22:41 +0300

    EP-847. Tests corrections and small additions.

--- tvb_epilepsy/tests/test_timeseries.py
+++ tvb_epilepsy/tests/test_timeseries.py
@@ -34,14 +34,14 @@ class TestTimeseries(object):
             Timeseries(data=self.data_1D, dimension_labels={}, time_start=0, time_step=0.01, time_unit="ms")
 
     def test_timeseries_2D(self):
-        ts_from_2D = Timeseries(self.data_2D, dimension_labels={TimeseriesDimensions.SPACE: ["r1", "r2", "r3"]},
+        ts_from_2D = Timeseries(self.data_2D, dimension_labels={TimeseriesDimensions.SPACE.value: ["r1", "r2", "r3"]},
                                 time_start=self.time_start, time_step=self.time_step, time_unit=self.time_unit)
         assert ts_from_2D.data.ndim == 4
         assert ts_from_2D.data.shape[2] == ts_from_2D.data.shape[3] == 1
         assert ts_from_2D.data.shape == (3, 3, 1, 1)
 
-        assert ts_from_2D.get_end_time() == 0.02
-        assert all(ts_from_2D.get_time_line() == numpy.array([0, 0.01, 0.02]))
+        assert ts_from_2D.end_time == 0.02
+        assert all(ts_from_2D.time_line == numpy.array([0, 0.01, 0.02]))
 
         with pytest.raises(KeyError):
             ts_from_2D.get_state_variable("")
@@ -53,8 +53,8 @@ class TestTimeseries(object):
         ts_r2 = ts_from_2D.get_subspace_by_labels(["r2"])
         assert ts_r2.data.ndim == 4
         assert ts_r2.data.shape == (3, 1, 1, 1)
-        assert ts_r2.dimension_labels[TimeseriesDimensions.SPACE] == ["r2"]
-        assert ts_r2.get_subspace_by_labels(["r2"]).dimension_labels[TimeseriesDimensions.SPACE] == ["r2"]
+        assert ts_r2.dimension_labels[TimeseriesDimensions.SPACE.value] == ["r2"]
+        assert ts_r2.get_subspace_by_labels(["r2"]).dimension_labels[TimeseriesDimensions.SPACE.value] == ["r2"]
 
         with pytest.raises(ValueError):
             ts_r2.get_subspace_by_labels(["r1"])
@@ -66,8 +66,8 @@ class TestTimeseries(object):
         ts_r2_idx = ts_r2r3_idx.get_subspace_by_index([0])
         assert ts_r2_idx.data.ndim == 4
         assert ts_r2_idx.data.shape == (3, 1, 1, 1)
-        assert ts_r2_idx.dimension_labels[TimeseriesDimensions.SPACE] == ["r2"]
-        assert ts_r2_idx.get_subspace_by_index([0]).dimension_labels[TimeseriesDimensions.SPACE] == ["r2"]
+        assert ts_r2_idx.dimension_labels[TimeseriesDimensions.SPACE.value] == ["r2"]
+        assert ts_r2_idx.get_subspace_by_index([0]).dimension_labels[TimeseriesDimensions.SPACE.value] == ["r2"]
 
         with pytest.raises(IndexError):
             ts_r2_idx.get_subspace_by_index([2])
@@ -78,13 +78,13 @@ class TestTimeseries(object):
         ts_time_window = ts_from_2D.get_time_window(1, 2)
         assert ts_time_window.data.ndim == 4
         assert ts_time_window.data.shape == (1, 3, 1, 1)
-        assert ts_time_window.dimension_labels[TimeseriesDimensions.SPACE] == ["r1", "r2", "r3"]
+        assert ts_time_window.dimension_labels[TimeseriesDimensions.SPACE.value] == ["r1", "r2", "r3"]
         assert ts_time_window.time_start == 0.01
 
         ts_time_window_units = ts_from_2D.get_time_window_by_units(0.01, 0.02)
         assert ts_time_window_units.data.ndim == 4
         assert ts_time_window_units.data.shape == (1, 3, 1, 1)
-        assert ts_time_window_units.dimension_labels[TimeseriesDimensions.SPACE] == ["r1", "r2", "r3"]
+        assert ts_time_window_units.dimension_labels[TimeseriesDimensions.SPACE.value] == ["r1", "r2", "r3"]
         assert ts_time_window_units.time_start == 0.01
 
         with pytest.raises(IndexError):
@@ -93,17 +93,21 @@ class TestTimeseries(object):
         with pytest.raises(ValueError):
             ts_from_2D.get_time_window_by_units(0, 0.025)
 
+        with pytest.raises(ValueError):
+            ts_from_2D.get_lfp()
+
     def test_timeseries_3D(self):
         ts_3D = Timeseries(self.data_3D,
-                           dimension_labels={TimeseriesDimensions.SPACE: [], TimeseriesDimensions.STATE_VARIABLES: []},
+                           dimension_labels={TimeseriesDimensions.SPACE.value: [],
+                                             TimeseriesDimensions.STATE_VARIABLES.value: []},
                            time_start=self.time_start, time_step=self.time_step, time_unit=self.time_unit)
         assert ts_3D.data.ndim == 4
         assert ts_3D.data.shape[3] == 1
 
     def test_timeseries_data_access(self):
         ts = Timeseries(self.data_3D,
-                        dimension_labels={TimeseriesDimensions.SPACE: ["r1", "r2", "r3", "r4"],
-                                          TimeseriesDimensions.STATE_VARIABLES: ["sv1", "sv2", "sv3"]},
+                        dimension_labels={TimeseriesDimensions.SPACE.value: ["r1", "r2", "r3", "r4"],
+                                          TimeseriesDimensions.STATE_VARIABLES.value: ["sv1", "sv2", "sv3"]},
                         time_start=self.time_start, time_step=self.time_step, time_unit=self.time_unit)
         assert isinstance(ts.r1, Timeseries)
         assert ts.r1.data.shape == (3, 1, 3, 1)
@@ -174,9 +178,17 @@ class TestTimeseries(object):
         with pytest.raises(IndexError):
             ts[0, :, 10, :]
 
+        with pytest.raises(ValueError):
+            ts.get_lfp()
+
+        with pytest.raises(ValueError):
+            ts.lfp
+
     def test_timeseries_4D(self):
         ts_4D = Timeseries(self.data_4D,
-                           dimension_labels={TimeseriesDimensions.SPACE: ["r1", "r2", "r3", "r4"],
-                                             TimeseriesDimensions.STATE_VARIABLES: ["sv1", "sv2", "sv3"]},
+                           dimension_labels={TimeseriesDimensions.SPACE.value: ["r1", "r2", "r3", "r4"],
+                                             TimeseriesDimensions.STATE_VARIABLES.value: ["y0", "y2", "sv3"]},
                            time_start=self.time_start, time_step=self.time_step, time_unit=self.time_unit)
         assert ts_4D.data.shape == (3, 4, 3, 4)
+        assert ts_4D.get_lfp().data.shape == (3, 4, 1, 4)
+        assert ts_4D.lfp.data.shape == ts_4D.get_lfp().data.shape
