eb1b63eca3b701983ac83a4f418c223b46a78e5b
dionperd <dionperd@gmail.com>
2018-04-19 14:37:09 +0200

    Added dictionary as group writing in h5Reader

--- tvb_epilepsy/io/h5_reader.py
+++ tvb_epilepsy/io/h5_reader.py
@@ -500,6 +500,15 @@ class H5Reader(object):
 
                     statistical_model.__setattr__(key, parameters)
 
+                if key == "ground_truth":
+                    ground_truth = OrderedDict()
+                    for group_key, group_value in value.iteritems():
+                        for dataset in group_value.keys():
+                            ground_truth[dataset] = group_value[group_key]
+                        for attr in group_value.attrs.keys():
+                            if attr not in H5_TYPES_ATTRUBUTES:
+                                ground_truth[attr] = group_value.attrs[attr]
+
                 if key == "active_regions":
                     statistical_model.active_regions = ensure_list(value)
 
