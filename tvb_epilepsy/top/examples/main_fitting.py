8033324f66aed678c56ea6bbe6d51091b500031d
dionperd <dionperd@gmail.com>
2018-04-24 18:26:19 +0200

    Testing

--- tvb_epilepsy/top/examples/main_fitting.py
+++ tvb_epilepsy/top/examples/main_fitting.py
@@ -244,7 +244,7 @@ if __name__ == "__main__":
         config.generic.CMDSTAN_PATH = "/WORK/episense/cmdstan-2.17.1"
 
     else:
-        output = os.path.join(user_home, 'Dropbox', 'Work', 'VBtech', 'VEP', "results", "fit2")
+        output = os.path.join(user_home, 'Dropbox', 'Work', 'VBtech', 'VEP', "results", "fit")
         config = Config(head_folder=head_folder, raw_data_folder=SEEG_data,
                         output_base=output, separate_by_run=False)
 
