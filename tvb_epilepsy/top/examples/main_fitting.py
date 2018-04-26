d5c53cabfb7c7a1b101bf6f0292ec5f342b4e8f0
dionperd <dionperd@gmail.com>
2018-04-26 19:27:03 +0200

    Ongoing work...

--- tvb_epilepsy/top/examples/main_fitting.py
+++ tvb_epilepsy/top/examples/main_fitting.py
@@ -247,7 +247,7 @@ if __name__ == "__main__":
         config.generic.CMDSTAN_PATH = "/WORK/episense/cmdstan-2.17.1"
 
     else:
-        output = os.path.join(user_home, 'Dropbox', 'Work', 'VBtech', 'VEP', "results", "fit_sim_source")
+        output = os.path.join(user_home, 'Dropbox', 'Work', 'VBtech', 'VEP', "results", "fit")
         config = Config(head_folder=head_folder, raw_data_folder=SEEG_data, output_base=output, separate_by_run=False)
 
     # TVB3 larger preselection:
