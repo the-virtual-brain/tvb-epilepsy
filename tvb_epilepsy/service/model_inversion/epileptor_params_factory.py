96028dc0310f50723651632e6421390adab88a0e
dionperd <dionperd@gmail.com>
2018-05-11 19:55:58 +0200

    Corrected lognormal distribution factory to determine scale from mean-low distance only

--- tvb_epilepsy/service/model_inversion/epileptor_params_factory.py
+++ tvb_epilepsy/service/model_inversion/epileptor_params_factory.py
@@ -1,6 +1,4 @@
 
-from collections import OrderedDict
-
 import numpy as np
 
 from tvb_epilepsy.base.computations.probability_distributions import ProbabilityDistributionTypes
@@ -10,7 +8,7 @@ from tvb_epilepsy.service.stochastic_parameter_builder import generate_stochasti
 
 def generate_lognormal_parameter(name, mean, low, high, sigma=None, sigma_scale=3, p_shape=(), use="scipy"):
     if sigma is None:
-        sigma = np.minimum(np.abs(high - mean), np.abs(mean - low)) / sigma_scale
+        sigma = np.abs(mean - low) / sigma_scale
     return generate_stochastic_parameter(name, low, high, loc=0.0, scale=1.0, p_shape=p_shape,
                                          probability_distribution=ProbabilityDistributionTypes.LOGNORMAL,
                                          optimize_pdf=True, use=use, **{"mean": mean/sigma, "skew": 0.0}). \
