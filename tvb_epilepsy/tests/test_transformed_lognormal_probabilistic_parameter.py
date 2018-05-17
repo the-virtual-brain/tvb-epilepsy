ed2168d7a6080c52dacc2ff2cd59095a441873a4
dionperd <dionperd@gmail.com>
2018-05-18 00:41:22 +0200

    lognormal generator without loc and scale, scale_sigma = 2 by default

--- tvb_epilepsy/tests/test_transformed_lognormal_probabilistic_parameter.py
+++ tvb_epilepsy/tests/test_transformed_lognormal_probabilistic_parameter.py
@@ -7,7 +7,7 @@ from tvb_epilepsy.tests.base import BaseTest
 if __name__ == "__main__":
 
     x0 = generate_negative_lognormal_parameter("x0", -2.5 * np.ones(2,), -4.0, 1.0,
-                                               sigma=None, sigma_scale=3,
+                                               sigma=None, sigma_scale=2,
                                                p_shape=(2,), use="scipy")
 
     Plotter(BaseTest.config).plot_probabilistic_parameter(x0, figure_name="test_transformed_probabilistic_parameter")
