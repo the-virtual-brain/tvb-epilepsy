6fc38518b85b3b4ea7504bcc66ee5bb03e5d6c7d
popaula937 <paula.popa@codemart.ro>
2018-06-12 10:59:04 +0300

    Test fix.

--- tvb_epilepsy/tests/test_plotter.py
+++ tvb_epilepsy/tests/test_plotter.py
@@ -1,11 +1,11 @@
 import os
 import numpy
+from tvb_epilepsy.base.model.timeseries import Timeseries, TimeseriesDimensions
 from tvb_epilepsy.plot.plotter import Plotter
 from tvb_epilepsy.service.simulator.epileptor_model_factory import build_EpileptorDP2D, VOIS
 from tvb_epilepsy.service.hypothesis_builder import HypothesisBuilder
 from tvb_epilepsy.service.model_configuration_builder import ModelConfigurationBuilder
 from tvb_epilepsy.service.probabilistic_parameter_builder import set_parameter
-from tvb_epilepsy.top.scripts.simulation_scripts import prepare_vois_ts_dict
 from tvb_epilepsy.tests.base import BaseTest
 
 
@@ -76,15 +76,19 @@ class TestPlotter(BaseTest):
         lsa_hypothesis = HypothesisBuilder(config=self.config).build_lsa_hypothesis()
         mc = ModelConfigurationBuilder().build_model_from_E_hypothesis(lsa_hypothesis, numpy.array([1]))
         model = build_EpileptorDP2D(mc)
-        res = prepare_vois_ts_dict(VOIS["EpileptorDP2D"], numpy.array([[[1, 2, 3], [1, 2, 3]], [[1, 2, 3], [1, 2, 3]]]))
-        res['time'] = numpy.array([1, 2, 3])
-        res['time_units'] = 'msec'
 
         # TODO: this figure_name is constructed inside plot method, so it can change
         figure_name = "EpileptorDP2D_Simulated_TAVG"
         file_name = os.path.join(self.config.out.FOLDER_FIGURES, figure_name + ".png")
         assert not os.path.exists(file_name)
 
-        self.plotter.plot_sim_results(model, [0], res)
+        data_3D = numpy.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, 1, 2]],
+                               [[3, 4, 5], [6, 7, 8], [9, 0, 1], [2, 3, 4]],
+                               [[5, 6, 7], [8, 9, 0], [1, 2, 3], [4, 5, 6]]])
+
+        self.plotter.plot_simulated_timeseries(
+            Timeseries(data_3D, {TimeseriesDimensions.SPACE.value: ["r1", "r2", "r3", "r4"],
+                                 TimeseriesDimensions.VARIABLES.value: ["x1", "x2", "z"]}, 0, 1),
+            model, [0])
 
         assert os.path.exists(file_name)
