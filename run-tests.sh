899f01c07896167bb33d9b5b80cf0e1fd3d1d683
popaula937 <paula.popa@codemart.ro>
2018-07-24 16:39:50 +0300

    EP-857. Fixed coverage module.

--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,7 @@ for ign in examples extern; do ignores="--ignore=$ign $ignores"; done
 
 # maybe do coverage
 cov=""
-if [[ $COV == "yes" ]]; then cov="--cov=tvb_epilepsy"; fi
+if [[ $COV == "yes" ]]; then cov="--cov=tvb_fit"; fi
 
 # run tests
 py.test --cov-config .coveragec $cov $ignores tvb_fit
