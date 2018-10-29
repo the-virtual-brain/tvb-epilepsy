a6c1ebc0abbc8ac54a442df0d08d24623c92719c
dionperd <dionperd@gmail.com>
2018-10-29 19:12:03 +0100

    Denis -> Dionysios

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 setuptools.setup(name='tvb_fit',
                  version='0.2',
                  description='TVB inference package and epilepsy applications',
-                 author='Denis Perdikis, Paula Popa, Lia Domide, Marmaduke Woodman',
+                 author='Dionysios Perdikis, Paula Popa, Lia Domide, Marmaduke Woodman',
                  author_email='<insert here>',
                  license="GPL v3",
                  url='https://github.com/the-virtual-brain/tvb-epilepsy',
