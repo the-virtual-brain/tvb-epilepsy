3924a58f995c44a87633f625ae6429e7a66ad112
dionperd <dionperd@gmail.com>
2018-11-06 11:58:32 -0800

    Adjusted main_fitting to mean centering and inclusion of K as a paramter

--- tvb_fit/tvb_epilepsy/top/examples/main_fitting.py
+++ tvb_fit/tvb_epilepsy/top/examples/main_fitting.py
@@ -9,7 +9,7 @@ from tvb_fit.base.utils.data_structures_utils import ensure_list, find_labels_in
 from tvb_fit.tvb_epilepsy.base.constants.config import Config
 from tvb_fit.tvb_epilepsy.base.constants.model_constants import K_UNSCALED_DEF, TAU1_DEF, TAU0_DEF
 from tvb_fit.tvb_epilepsy.base.constants.model_inversion_constants import XModes, SDE_MODES, \
-    OBSERVATION_MODELS, OBSERVATION_MODEL_DEF, SIGMA_DEF
+    OBSERVATION_MODELS, OBSERVATION_MODEL_DEF
 from tvb_fit.tvb_epilepsy.service.hypothesis_builder import HypothesisBuilder
 from tvb_fit.tvb_epilepsy.service.probabilistic_models_builders import SDEProbabilisticModelBuilder
 from tvb_fit.tvb_epilepsy.service.model_inversion_services import SDEModelInversionService
@@ -154,7 +154,7 @@ def main_fit_sim_hyplsa(stan_model_name, empirical_files, times_on, time_length,
         #---------------------------------Finally set priors for the parameters-------------------------------------
         probabilistic_model = \
                 set_prior_parameters(probabilistic_model, target_data, source2D_ts, None, problstc_model_file,
-                                     [XModes.X0MODE.value, "x1_init", "z_init", "tau1",  # "tau0", "K", "x1", "z"
+                                     [XModes.X0MODE.value, "x1_init", "z_init", "K", "tau1",  # "tau0", "x1", "z"
                                       "sigma", "dWt", "epsilon", "scale", "offset"], normal_flag,
                                       writer=writer, plotter=plotter)
 
@@ -174,9 +174,9 @@ def main_fit_sim_hyplsa(stan_model_name, empirical_files, times_on, time_length,
 
     estimates, samples, summary, info_crit = \
         run_fitting(probabilistic_model, stan_model_name, model_data, target_data, config, head,
-                    hyp.all_disease_indices, ["tau1", "sigma", "epsilon", "scale", "offset"], ["x0", "PZ", "x1eq", "zeq"],
-                    fit_flag, test_flag, base_path, fitmethod, n_chains_or_runs=2,
-                    output_samples=100, num_warmup=50, min_samples_per_chain=100, max_depth=15, delta=0.95,
+                    hyp.all_disease_indices, ["K", "tau1", "sigma", "epsilon", "scale", "offset"],
+                    ["x0", "PZ", "x1eq", "zeq"], fit_flag, test_flag, base_path, fitmethod, n_chains_or_runs=2,
+                    output_samples=100, num_warmup=100, min_samples_per_chain=100, max_depth=15, delta=0.95,
                     iter=500000, tol_rel_obj=1e-6, debug=1, simulate=0, writer=writer, plotter=plotter, **kwargs)
 
 
@@ -206,7 +206,7 @@ if __name__ == "__main__":
 
     else:
         output = os.path.join(user_home, 'Dropbox', 'Work', 'VBtech', 'VEP', "results",
-                              "fit/tests/empirical_convolve_log_singlestep")
+                              "fit/tests/simsensor_convolve_log_singlestep_meancntrd")
         config = Config(head_folder=head_folder, raw_data_folder=SEEG_data, output_base=output, separate_by_run=False)
         config.generic.CMDSTAN_PATH = config.generic.CMDSTAN_PATH + "_precompiled"
     config.generic.PROBLSTC_MODELS_PATH = os.path.join(user_home, "VEPlocal/CC/tvb-epilepsy-cc-study/stan")
@@ -224,7 +224,7 @@ if __name__ == "__main__":
     # Simulation times_on_off
     #  for "fitting" simulations with tau0=30.0
     sim_times_on_off = [70.0, 120.0] # e_hypo, [100, 130] for x0_hypo, and e_x0_hypo
-    EMPIRICAL = True
+    EMPIRICAL = False
     sim_source_type = "paper"
     observation_model = OBSERVATION_MODELS.SEEG_LOGPOWER.value  #OBSERVATION_MODELS.SEEG_LOGPOWER.value  #OBSERVATION_MODELS.SOURCE_POWER.value  #
     if EMPIRICAL:
@@ -254,7 +254,7 @@ if __name__ == "__main__":
     preprocessing = []
     downsampling = 2
     normal_flag = False
-    stan_model_name = "vep_sde_cc"
+    stan_model_name = "vep_sde_cc_meancntrd"
     fitmethod = "sample"   # ""  # "sample"  # "advi" or "opt"
     pse_flag = True
     fit_flag = True
