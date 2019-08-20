d5c5b33d4b111c3d23047a0e8853ad40210fec30
Dionysios Perdikis <dionperd@gmail.com>
2019-08-20 20:00:33 +0200

    Using duplicate for get_source(). get_state_variable->get_variables

--- tvb_fit/tvb_epilepsy/base/model/timeseries.py
+++ tvb_fit/tvb_epilepsy/base/model/timeseries.py
@@ -1,6 +1,7 @@
-
+from copy import deepcopy
 from enum import Enum
-from collections import OrderedDict
+
+import numpy as np
 
 from tvb_utils.log_error_utils import initialize_logger
 from tvb_timeseries.model.timeseries import Timeseries as TimeseriesBase, TimeseriesDimensions
@@ -33,25 +34,22 @@ class Timeseries(TimeseriesBase):
             raise ValueError
 
         if PossibleVariables.SOURCE.value in self.labels_dimensions[TimeseriesDimensions.VARIABLES.value]:
-            return self.get_state_variable(PossibleVariables.SOURCE.value)
+            return self.get_variables(PossibleVariables.SOURCE.value)
         if PossibleVariables.X1.value in self.labels_dimensions[TimeseriesDimensions.VARIABLES.value]:
-            y0_ts = self.get_state_variable(PossibleVariables.X1.value)
+            y0_ts = self.get_variables(PossibleVariables.X1.value)
             if PossibleVariables.X2.value in self.labels_dimensions[TimeseriesDimensions.VARIABLES.value]:
                 self.logger.info("%s is computed using %s and %s state variables!" % (
                     PossibleVariables.SOURCE.value, PossibleVariables.X1.value, PossibleVariables.X2.value))
-                y2_ts = self.get_state_variable(PossibleVariables.X2.value)
+                y2_ts = self.get_variables(PossibleVariables.X2.value)
                 source_data = y2_ts.data - y0_ts.data
             else:
                 self.logger.warn("%s is computed using %s state variable!" % (
                     PossibleVariables.SOURCE.value, PossibleVariables.X1.value))
                 source_data = -y0_ts.data
-            source_dim_labels = {
-                 TimeseriesDimensions.SPACE.value: self.labels_dimensions[TimeseriesDimensions.SPACE.value],
-                 TimeseriesDimensions.VARIABLES.value: [PossibleVariables.SOURCE.value]}
-            return Timeseries(  # substitute with TimeSeriesRegion fot TVB like functionality
-                              source_data, start_time=self.start_time, connectivity=self.connectivity,
-                              labels_ordering=self.labels_ordering, labels_dimensions=source_dim_labels,
-                              sample_period=self.sample_period, sample_period_unit=self.time_unit, ts_type=self.ts_type)
+            source_dim_labels = deepcopy(self.labels_dimensions)
+            source_dim_labels[self.labels_ordering[1]] = np.array([PossibleVariables.SOURCE.value])
+            return self.duplicate(data=source_data, labels_dimensions=source_dim_labels)
+
         self.logger.error(
             "%s is not computed and cannot be computed now because state variables %s and %s are not defined!" % (
                 PossibleVariables.SOURCE.value, PossibleVariables.X1.value, PossibleVariables.X2.value))
