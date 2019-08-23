e6e4f2082cc8baaf2d767d94a395158583466fd7
Dionysios Perdikis <dionperd@gmail.com>
2019-08-23 02:45:21 +0200

    Added main_workflow back

--- tvb_fit/tvb_epilepsy/top/examples/main_workflow.py
+++ tvb_fit/tvb_epilepsy/top/examples/main_workflow.py
@@ -9,11 +9,12 @@ if __name__ == "__main__":
 
     user_home = os.path.expanduser("~")
     SUBJECT = "TVB3"
-    head_folder = os.path.join(user_home, 'Dropbox/Work/VBtech/VEP/results/tests/workflow', SUBJECT, "HeadD")
-    SEEG_data = os.path.join(os.path.expanduser("~"), 'Dropbox', 'Work', 'VBtech', 'VEP', "data/CC", "seeg", SUBJECT)
-    output = os.path.join(user_home, 'Dropbox/Work/VBtech/VEP/results/tests/workflow', SUBJECT, "HeadD")
+    DROPBOX_HOME = os.path.join(user_home, 'Dropbox', 'Work', 'VBtech', 'VEP')
+    head_folder = os.path.join(DROPBOX_HOME, "results", "CC", SUBJECT, "HeadD")
+    SEEG_data = os.path.join(DROPBOX_HOME, "data/CC", "seeg", SUBJECT)
+    output = os.path.join(user_home, 'Dropbox/Work/VBtech/VEP/results/tests/workflow')
     config = Config(head_folder=head_folder, raw_data_folder=SEEG_data, output_base=output, separate_by_run=False)
-    config.generic.CMDSTAN_PATH = config.generic.CMDSTAN_PATH + "_precompiled"
+    config.generic.CMDSTAN_PATH = config.generic.CMDSTAN_PATH
 
     wf = Workflow(config)
     wf.read_head()
